2f8fd8562a3f59edb8e58d77523ec3ef7e790b8e
Yo-An Lin <yoanlin93@gmail.com>
2012-02-02 16:49:15 +0800

    Add basic sqlite sql schema builder.

--- src/LazyRecord/SchemaSqlBuilder/SqliteDriver.php
+++ src/LazyRecord/SchemaSqlBuilder/SqliteDriver.php
@@ -6,9 +6,65 @@ class SqliteDriver
 	implements DriverInterface
 {
 
+
+    function buildColumnSql($column) {      
+		$name = $column->name;
+		$isa  = $column->isa ?: 'str';
+		$type = $column->type;
+		if( ! $type && $isa == 'str' )
+			$type = 'text';
+
+		$sql  = $name;
+		$sql .= ' ' . $type;
+
+        if( $column->required )
+            $sql .= " not null";
+
+		/* if it's callable, we should not write the result into sql schema */
+		if( ($default = $column->default) !== null && ! is_callable($column->default )  ) { 
+
+			if( is_string($default) ) {
+				$sql .= " default '" . addslashes($default) . "'";
+			}
+			elseif( is_numeric($default) ) { 
+				$sql .= " default $default";
+			}
+			elseif( is_bool($default) ) {
+				$sql .= ' default ' . ( $default === true ? 'TRUE' : 'FALSE' );
+			}
+			elseif( is_array($default) ) {
+				// raw sql default value
+				$sql .= " default " . $default[0];
+			}
+			else {
+				$sql .= " default $default";
+			}
+		}
+
+        if( $column->primary )
+            $sql .= ' primary key';
+        
+        if( $column->autoIncrement )
+            $sql .= ' autoincrement';
+
+		if( $column->unique )
+			$sql .= ' unique';
+
+        return $sql;
+    }
+
+
 	public function build(SchemaDeclare $schema)
 	{
-		return '';
+
+        $sql = "CREATE TABLE " . $schema->getTable() . "( \n";
+        $columnSql = array();
+        foreach( $schema->columns as $name => $column ) {
+			$columnSql[] = $this->buildColumnSql( $column );
+        }
+        $sql .= join(",\n",$columnSql);
+        $sql .= "\n);\n";
+        return $sql;
 	}
 
 }
--- tests/LazyRecord/SchemaSqlBuilderTest.php
+++ tests/LazyRecord/SchemaSqlBuilderTest.php
@@ -8,15 +8,36 @@ class SchemaSqlBuilderTest extends PHPUnit_Framework_TestCase
 	{
 		$builder = new SchemaSqlBuilder('sqlite');
 		ok( $builder );
+
+		$s = new \tests\AuthorSchema;
+		$s->build();
+		ok( $s );
+
+		$sql = $builder->build($s);
+		ok( $sql );
+
+		var_dump( $sql ); 
+
+		if( file_exists('tests.db') ) {
+			unlink('tests.db');
+		}
+
+		$dbh = new PDO('sqlite:tests.db'); // success
+		$ret = $dbh->query( $sql );
+		ok( $ret );
+
+		$error = $dbh->errorInfo();
+		if($error[1] != null ) {
+			throw new Exception('DATABASE CONNECTION ERROR');
+		}
+
+
 	}
 
 	function testMysql()
 	{
 		$builder = new SchemaSqlBuilder('mysql');
 		ok( $builder );
-
-		$s = new \tests\AuthorSchema;
-		ok( $s );
 
 	}
 }
