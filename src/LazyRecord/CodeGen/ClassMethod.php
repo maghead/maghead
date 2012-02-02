d1d21fc2036ff779f60ebfa00919f579e31821f1
Yo-An Lin <yoanlin93@gmail.com>
2012-02-02 15:33:22 +0800

    seperate class files

---
+++ src/LazyRecord/CodeGen/ClassMethod.php
@@ -0,0 +1,32 @@
+<?php
+namespace LazyRecord\CodeGen;
+
+class ClassMethod
+{
+    public $name;
+    public $scope = 'public';
+    public $code;
+    public $arguments = array();
+
+    public function __construct($name,$arguments = array())
+    {
+        $this->name = $name;
+        $this->arguments = $arguments;
+    }
+
+    public function __toString()
+    {
+        $argStrings = array();
+        foreach( $this->arguments as $name => $default ) {
+            $argStrings[] = " \$$name = " . var_export( $default,true);
+        }
+        
+        return $this->scope . ' function ' . $this->name . '(' . join(', ', $argStrings) . ')' . " { " . "\n"
+            . $this->code
+            . "\n"
+            . "}"
+            . "\n"
+        ;
+    }
+}
+
