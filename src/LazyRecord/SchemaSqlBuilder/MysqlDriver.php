2e6a63492ba6084f23391b64e2a18a3b3f3fc55b
Yo-An Lin <yoanlin93@gmail.com>
2012-02-02 17:56:53 +0800

    more types

--- src/LazyRecord/SchemaSqlBuilder/MysqlDriver.php
+++ src/LazyRecord/SchemaSqlBuilder/MysqlDriver.php
@@ -6,12 +6,90 @@ class MysqlDriver
 	implements DriverInterface
 {
 
+    function buildColumnSql($schema, $column) {      
+		$name = $column->name;
+		$isa  = $column->isa ?: 'str';
+		$type = $column->type;
+		if( ! $type && $isa == 'str' )
+			$type = 'text';
+
+		$sql  = $name;
+		$sql .= ' ' . $type;
+
+        if( $column->required )
+            $sql .= " not null";
+
+		/* if it's callable, we should not write the result into sql schema */
+		if( ($default = $column->default) !== null && ! is_callable($column->default )  ) { 
+
+			if( is_string($default) ) {
+				$sql .= " default '" . addslashes($default) . "'";
+			}
+			elseif( is_numeric($default) ) { 
+				$sql .= " default $default";
+			}
+			elseif( is_bool($default) ) {
+				$sql .= ' default ' . ( $default === true ? 'TRUE' : 'FALSE' );
+			}
+			elseif( is_array($default) ) {
+				// raw sql default value
+				$sql .= " default " . $default[0];
+			}
+			else {
+				$sql .= " default $default";
+			}
+		}
+
+        if( $column->primary )
+            $sql .= ' primary key';
+        
+        if( $column->autoIncrement )
+            $sql .= ' auto_increment';
+
+		if( $column->unique )
+			$sql .= ' unique';
+
+		// build reference
+        // track(
+		//		FOREIGN KEY(trackartist) REFERENCES artist(artistid)
+		//		artist_id INTEGER REFERENCES artist
+        // )
+		foreach( $schema->relations as $rel ) {
+			switch( $rel['type'] ) {
+
+                // XXX: keep this
+                case SchemaDeclare::belongs_to:
+                    var_dump( $rel ); 
+                    $fs = new $rel['foreign']['schema'];
+                    $fcName = $rel['foreign']['column'];
+                    $fc = $fs->columns[$fcName];
+                break;
+
+				case SchemaDeclare::has_one:
+					if( $rel['self']['column'] == $name ) { 
+						$fs = new $rel['foreign']['schema'];
+						$sql .= ' references ' . $fs->getTable();
+					}
+				break;
+			}
+		}
+
+        return $sql;
+    }
+
+
 
 	public function build(SchemaDeclare $schema)
 	{
 
-
-
+        $sql = "CREATE TABLE " . $schema->getTable() . "( \n";
+        $columnSql = array();
+        foreach( $schema->columns as $name => $column ) {
+			$columnSql[] = $this->buildColumnSql( $schema, $column );
+        }
+        $sql .= join(",\n",$columnSql);
+        $sql .= "\n);\n";
+        return $sql;
 	}
 
 }
--- tests/LazyRecord/SchemaSqlBuilderTest.php
+++ tests/LazyRecord/SchemaSqlBuilderTest.php
@@ -7,10 +7,13 @@ class SchemaSqlBuilderTest extends PHPUnit_Framework_TestCase
     function pdoQueryOk($dbh,$sql)
     {
 		$ret = $dbh->query( $sql );
-		ok( $ret );
+
 		$error = $dbh->errorInfo();
 		if($error[1] != null ) {
-			throw new Exception( var_export( $error, true ) );
+            throw new Exception( 
+                var_export( $error, true ) 
+                . ' SQL: ' . $sql 
+            );
 		}
         // ok( $error[1] != null );
         return $ret;
@@ -27,39 +30,65 @@ class SchemaSqlBuilderTest extends PHPUnit_Framework_TestCase
 		ok( $builder );
 
 		$s = new \tests\AuthorSchema;
+		$authorbook = new \tests\AuthorBookSchema;
+		$bookschema = new \tests\BookSchema;
 		ok( $s );
 
 		$sql = $builder->build($s);
 		ok( $sql );
-        var_dump( $sql ); 
+        // var_dump( $sql ); 
         $this->pdoQueryOk( $dbh , $sql );
 
 
-		$authorbook = new \tests\AuthorBookSchema;
 		ok( $authorbook );
 		$sql = $builder->build($authorbook);
 		ok( $sql );
-        var_dump( $sql ); 
+        // var_dump( $sql ); 
 
         $this->pdoQueryOk( $dbh , $sql );
 
 
-		$bookschema = new \tests\BookSchema;
 		ok( $bookschema );
 		$sql = $builder->build($bookschema);
 		ok( $sql );
-        var_dump( $sql ); 
+        // var_dump( $sql ); 
 
         $this->pdoQueryOk( $dbh , $sql );
-
-
-
 	}
+
 
 	function testMysql()
 	{
 		$builder = new SchemaSqlBuilder('mysql');
 		ok( $builder );
+
+        $pdo = new PDO( 
+            'mysql:host=localhost;dbname=lazy_tests', 
+            'root', 
+            '123123', 
+            array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8") 
+        ); 
+        ok( $pdo , 'pdo connection' );
+
+        $this->pdoQueryOk( $pdo, 'drop TABLE IF EXISTS authors' );
+        $this->pdoQueryOk( $pdo, 'drop TABLE IF EXISTS author_books' );
+        $this->pdoQueryOk( $pdo, 'drop TABLE IF EXISTS books' );
+
+		$authorschema = new \tests\AuthorSchema;
+		$authorbookschema = new \tests\AuthorBookSchema;
+		$bookschema = new \tests\BookSchema;
+        ok( $authorschema );
+        ok( $authorbookschema );
+        ok( $bookschema );
+
+        ok( $sql = $builder->build( $authorschema ) );
+        $this->pdoQueryOk( $pdo, $sql );
+
+        ok( $sql = $builder->build( $bookschema ) );
+        $this->pdoQueryOk( $pdo, $sql );
+
+        ok( $sql = $builder->build( $authorbookschema ) );
+        $this->pdoQueryOk( $pdo, $sql );
 
 	}
 }
