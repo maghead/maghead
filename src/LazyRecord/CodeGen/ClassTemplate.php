34ebdc74a2f2f49becb41668f075050dc58fcb44
c9s <cornelius.howl@gmail.com>
2012-02-02 15:35:04 +0800

    UseClass.

--- src/LazyRecord/CodeGen/ClassTemplate.php
+++ src/LazyRecord/CodeGen/ClassTemplate.php
@@ -1,29 +1,6 @@
 <?php
 namespace LazyRecord\CodeGen;
 
-
-class UseClass
-{
-	public $as;
-	public $class;
-
-	public function __construct($class,$as = null)
-	{
-		$this->class = ltrim( $class , '\\' );
-		$this->as = $as ? ltrim($as,'\\') : null;
-	}
-
-	public function __toString()
-	{
-		$code = 'use ' . $this->class;
-		if( $this->as ) {
-			$code .= ' ' . $this->as . ';';
-		}
-		return $code;
-	}
-
-}
-
 class ClassTemplate
 {
     public $class;
