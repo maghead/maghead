0f5423fa2a4da35801a125be7ec45c0ce95ba558
Yo-An Lin <yoanlin93@gmail.com>
2012-02-04 01:58:15 +0800

    Checking files

---
+++ src/LazyRecord/Deflator.php
@@ -0,0 +1,52 @@
+<?php
+namespace LazyRecord;
+use DateTime;
+
+class Deflator
+{
+
+
+    /** 
+     * provide a custom deflator for data type
+     * xxx:
+     */
+    static function register($dataType)
+    {
+
+    }
+
+    static function deflate($value,$dataType)
+    {
+        if( $value === null )
+            return $value;
+
+        /* respect the data type to inflate value */
+        if( $dataType == 'int' ) {
+            return (int) $value;
+        }
+        elseif( $dataType == 'str' ) {
+            return (string) $value;
+        }
+        elseif( $dataType == 'bool' ) {
+            if( strcasecmp( 'false', $value ) == 0 || $value == '0' ) {
+                return false;
+            } 
+            elseif( strcasecmp( 'true', $value ) == 0 || $value == '1' ) {
+                return true;
+            }
+            return $value ? true : false;
+        }
+        elseif( $dataType == 'float' ) {
+            return (float) $value;
+        }
+        elseif( $dataType == 'DateTime' ) {
+            return new DateTime( $value );
+        }
+        return $value;
+    }
+
+}
+
+
+
+
