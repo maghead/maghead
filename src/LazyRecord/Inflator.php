a0ebf3ba0a889bff55fe2a43fc4a724fb8a69602
c9s <cornelius.howl@gmail.com>
2012-02-04 01:58:15 +0800

    Checking files

---
+++ src/LazyRecord/Inflator.php
@@ -0,0 +1,29 @@
+<?php
+namespace LazyRecord;
+
+/**
+ * inflate value into database
+ */
+class Inflator
+{
+
+    static function inflate($value, $dataType)
+    {
+        /* respect the data type to inflate value */
+        if( $dataType == 'int' ) {
+            return (int) $value;
+        }
+        elseif( $dataType == 'str' ) {
+            return (string) $value;
+        }
+        elseif( $dataType == 'bool' ) {
+            return (boolean) $value;
+        }
+        elseif( $dataType == 'float' ) {
+            return (float) $value;
+        }
+        return $value;
+    }
+
+}
+
