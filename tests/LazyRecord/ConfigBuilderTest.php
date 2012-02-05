ecddc13f54cc6fe3fde66e1a43866070f64e687d
Yo-An Lin <yoanlin93@gmail.com>
2012-02-05 16:25:10 +0800

    Add test files

---
+++ tests/LazyRecord/ConfigBuilderTest.php
@@ -0,0 +1,23 @@
+<?php
+
+class ConfigBuilderTest extends PHPUnit_Framework_TestCase
+{
+    function test()
+    {
+        $builder = new LazyRecord\ConfigBuilder;
+        ok($builder);
+
+        $builder->read('tests/lazy.yml');
+        $builder->validate();
+        $content = $builder->build();
+        ok( $content );
+
+        file_put_contents('tests/lazy-config.php'
+                ,$content);
+
+
+        $loader = new LazyRecord\ConfigLoader;
+        $loader->load( 'tests/lazy-config.php');
+    }
+}
+
