f00b6d3c5b87191e4a065406a4e545a7a14e4230
c9s <cornelius.howl@gmail.com>
2012-02-05 15:19:51 +0800

    Register commands

--- src/LazyRecord/Console.php
+++ src/LazyRecord/Console.php
@@ -4,15 +4,11 @@ use CLIFramework\Application;
 
 class Console extends Application
 {
-
 	public function init()
 	{
 		parent::init();
+        $this->registerCommand('build-conf', 'BuildConfCommand');
+        $this->registerCommand('schema', 'BuildSchemaCommand');
+        $this->registerCommand('sql', 'BuildSqlCommand');
 	}
-
-	public function execute()
-	{
-
-	}
-
 }
