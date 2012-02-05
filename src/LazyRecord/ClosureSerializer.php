d864d627c96e78b2e3823578fad836771a9a9c8d
c9s <cornelius.howl@gmail.com>
2012-02-05 20:18:15 +0800

    Add closure serializer

---
+++ src/LazyRecord/ClosureSerializer.php
@@ -0,0 +1,36 @@
+<?php
+namespace LazyRecord;
+use ReflectionFunction;
+use SplFileObject;
+
+class ClosureSerializer
+{
+    static function serialize($closure)
+    {
+        $ref = new ReflectionFunction($closure);
+        /*
+        var_dump(
+            $ref->getName(), 
+            $ref->getNumberOfParameters(), 
+            $ref->getNumberOfRequiredParameters()
+        );
+         */
+
+        $file = new SplFileObject($ref->getFileName());
+        $file->seek($ref->getStartLine()-1);
+        $code = '';
+        while ($file->key() < $ref->getEndLine())
+        {
+            $code .= $file->current();
+            $file->next();
+        }
+        $start = strpos($code, 'function');
+        $end = strrpos($code, '}') + 1;
+        return substr($code, $start, $end - $start);
+    }
+}
+
+
+
+
+
