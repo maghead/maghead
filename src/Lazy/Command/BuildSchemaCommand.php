ada691e785aac93281bb31ea2210d1d9d7957de6
Yo-An Lin <yoanlin93@gmail.com>
2012-02-06 13:47:57 +0800

    build-schema command

--- src/Lazy/Command/BuildSchemaCommand.php
+++ src/Lazy/Command/BuildSchemaCommand.php
@@ -1,10 +1,37 @@
 <?php
 namespace Lazy\Command;
 
+
+/**
+ *
+ * $ lazy build-schema path/to/Schema path/to/SchemaDir
+ *
+ */
 class BuildSchemaCommand extends \CLIFramework\Command
 {
     public function execute()
     {
+		$generator = new \Lazy\SchemaGenerator;
+		$generator->setLogger( $this->getLogger() );
 
+
+
+        $args = func_get_args();
+        if( count($args) ) {
+            foreach( $args as $path ) {
+                $generator->addPath( $path );
+            }
+        } else {
+            // XXX: build from config
+        
+        }
+
+        $classMap = $generator->generate();
+		foreach( $classMap as $class => $file ) {
+			// path_ok( $file , $class );
+#  			unlink( $file );
+		}
+
+        $this->getLogger()->info('Done');
     }
 }
