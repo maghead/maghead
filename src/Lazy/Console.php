88c1317aad3d496516c48f1ad076b3a50a3eb317
c9s <cornelius.howl@gmail.com>
2012-02-09 22:15:16 +0800

    Add init-conf

--- src/Lazy/Console.php
+++ src/Lazy/Console.php
@@ -7,6 +7,7 @@ class Console extends Application
 	public function init()
 	{
 		parent::init();
+        $this->registerCommand('init-conf',    'Lazy\Command\InitConfCommand');
         $this->registerCommand('build-conf',   'Lazy\Command\BuildConfCommand');
         $this->registerCommand('build-schema', 'Lazy\Command\BuildSchemaCommand');
         $this->registerCommand('build-sql',    'Lazy\Command\BuildSqlCommand');
