b10962c04e7b1ca6ab0db3bd1e490582546227c2
Yo-An Lin <yoanlin93@gmail.com>
2012-02-09 22:14:46 +0800

    Add InitConfCommand.

---
+++ src/Lazy/Command/InitConfCommand.php
@@ -0,0 +1,47 @@
+<?php
+namespace Lazy\Command;
+use Exception;
+use CLIFramework\Command;
+
+class InitConfCommand extends Command
+{
+
+    public function brief()
+    {
+        return 'init configuration file.';
+    }
+
+    public function execute()
+    {
+        /**
+         * $ lazy bulid-conf config/lazy.yml phifty/config/lazy.yml
+         * 
+         * build/lazy/config.php   # is generated
+         */
+        $options = $this->getOptions();
+        $logger = $this->getLogger();
+
+        $configFile = 'config/lazy.yml';
+        $logger->info("Creating config skeleton...");
+
+        $content =<<<EOS
+---
+bootstrap:
+  - tests/bootstrap.php
+schema:
+  paths:
+    - tests/schema
+data_sources:
+  default:
+    dsn: 'sqlite:tests.db'
+    # dsn: 'sqlite::memory:'
+  mysql:
+    dsn: 'mysql:host=localhost;dbname=test'
+    user: root
+    pass: 123123
+EOS;
+        if( file_put_contents( $configFile , $content ) !== false ) {
+            $logger->info("Config file is generated at: $configFile");
+        }
+    }
+}
