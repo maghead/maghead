f685a43517c8d7aaff569613fba10b96585ccc33
c9s <cornelius.howl@gmail.com>
2012-02-12 18:09:31 +0800

    - update document.
    - trigger beforeUpdate, afterUpdate.

--- src/Lazy/BaseModel.php
+++ src/Lazy/BaseModel.php
@@ -67,7 +67,25 @@ class BaseModel
         ));
     }
 
+    public function beforeDelete( $args )
+    {
+        return $args;
+    }
 
+    public function afterDelete( $args )
+    {
+
+    }
+
+    public function beforeUpdate( $args )
+    {
+        return $args;
+    }
+
+    public function afterUpdate( $args )
+    {
+
+    }
 
     public function beforeCreate( $args ) 
     {
@@ -89,6 +107,8 @@ class BaseModel
      * create a new record
      *
      * @param array $args data
+     *
+     * @return OperationResult operation result (success or error)
      */
     protected function _create($args)
     {
@@ -140,6 +160,7 @@ class BaseModel
     /**
      * delete current record, the record should be loaded already.
      *
+     * @return OperationResult operation result (success or error)
      */
     public function _delete()
     {
@@ -171,14 +192,18 @@ class BaseModel
      * update current record
      *
      * @param array $args
+     *
+     * @return OperationResult operation result (success or error)
      */
     public function _update( $args ) 
     {
+        // check if the record is loaded.
         $k = $this->_schema->primaryKey;
         if( $k && ! isset($args[ $k ]) && ! isset($this->_data[$k]) ) {
             return $this->reportError('Record is not loaded, Can not update record.');
         }
 
+        // check if we get primary key value
         $kVal = isset($args[$k]) 
             ? $args[$k] : isset($this->_data[$k]) 
             ? $this->_data[$k] : null;
@@ -191,9 +216,9 @@ class BaseModel
 #          if( $result )
 #              return $result;
 
+        $args = $this->beforeUpdate($args);
 
-#          $args = $this->beforeUpdate( $args );
-#          $args = $this->deflateData( $args ); // apply args to columns
+        // $args = $this->deflateData( $args ); // apply args to columns
 
         $query = $this->createQuery();
         $query->update($args)->where()
@@ -213,6 +238,8 @@ class BaseModel
         // merge updated data
         $this->_data = array_merge($this->_data,$args);
 
+        $this->afterUpdate($args);
+
         // throw new Exception( "Update failed." . $dbc->error );
         $result = new OperationSuccess;
         $result->id = $kVal;
@@ -225,12 +252,14 @@ class BaseModel
      * Save current data (create or update)
      * if primary key is defined, do update
      * if primary key is not defined, do create
+     *
+     * @return OperationResult operation result (success or error)
      */
     public function save()
     {
         $k = $this->_schema->primaryKey;
         $doCreate = ( $k && ! isset($this->_data[$k]) );
-        return $doCreate 
+        return $doCreate
             ? $this->create( $this->_data )
             : $this->update( $this->_data );
     }
@@ -243,6 +272,9 @@ class BaseModel
      * for datetime object, deflate it into DateTime object.
      * for integer  object, deflate it into int type.
      * for boolean  object, deflate it into bool type.
+     *
+     * @param array $args
+     * @return array current record data.
      */
     public function deflateData(& $args) {
         foreach( $args as $k => $v ) {
@@ -253,7 +285,10 @@ class BaseModel
         return $args;
     }
 
-
+    /**
+     * deflate current record data, usually deflate data from database 
+     * turns data into objects, int, string (type casting)
+     */
     public function deflate()
     {
         $this->deflateData( $this->_data );
@@ -277,7 +312,6 @@ class BaseModel
             break;
 
             case self::has_many:
-
             break;
         }
     }
@@ -287,6 +321,13 @@ class BaseModel
      * get pdo connetion and make a query
      *
      * @param string $sql SQL statement
+     *
+     * @return PDOStatement pdo statement object.
+     *
+     *     $stm = $this->dbQuery($sql);
+     *     foreach( $stm as $row ) {
+     *              $row['name'];
+     *     }
      */
     public function dbQuery($sql)
     {
@@ -296,6 +337,12 @@ class BaseModel
     }
 
 
+    // xxx: process for read/write source
+    public function getDataSourceId()
+    {
+        return 'default';
+    }
+
     /**
      * get default connection object (PDO) from connection manager
      *
@@ -303,10 +350,9 @@ class BaseModel
      */
     public function getConnection()
     {
-        // xxx: process for read/write source
-        $sourceId = 'default';
+        $sourceId = $this->getDataSourceId();
         $connManager = ConnectionManager::getInstance();
-        return $connManager->getDefault(); // xxx: support read/write connection later
+        return $connManager->getConnection( $sourceId ); // xxx: support read/write connection later
     }
 
 
@@ -335,24 +381,33 @@ class BaseModel
         return isset($this->_data[ $name ] );
     }
 
-    public function resetData()
+    /**
+     * clear current data stash
+     */
+    public function clearData()
     {
         $this->_data = array();
     }
 
+
+    /**
+     * get current record data stash
+     *
+     * @return array record data stash
+     */
     public function getData()
     {
         return $this->_data;
     }
 
-
-
-
     /**
      * Handle static calls for model class.
      *
      * ModelName::delete()
-     *     ->where();
+     *     ->where()
+     *       ->equal('id', 3)
+     *       ->back()
+     *      ->execute();
      *
      * ModelName::update( $hash )
      *     ->where()
@@ -389,6 +444,12 @@ class BaseModel
         }
     }
 
+    /**
+     * Create new record with data array
+     *
+     * @param array $args data array.
+     * @return BaseModel $record
+     */
     public static function __static_create($args)
     {
         $model = new static;
@@ -396,6 +457,17 @@ class BaseModel
         return $model;
     }
 
+    /**
+     * Update record with data array
+     *
+     * @return SQLBuilder\Expression expression for building where condition sql.
+     *
+     * Model::update(array( 'name' => 'New name' ))
+     *     ->where()
+     *       ->equal('id', 1)
+     *       ->back()
+     *     ->execute();
+     */
     public static function __static_update($args) 
     {
         $model = new static;
@@ -414,6 +486,18 @@ class BaseModel
         return $query;
     }
 
+
+    /**
+     * static delete action
+     *
+     * @return SQLBuilder\Expression expression for building delete condition.
+     *
+     * Model::delete()
+     *    ->where()
+     *       ->equal( 'id' , 3 )
+     *       ->back()
+     *       ->execute();
+     */
     public static function __static_delete()
     {
         $model = new static;
