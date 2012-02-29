25717af83edd41a2155bec03caef06cdbeeb67cd
c9s <cornelius.howl@gmail.com>
2012-02-29 20:24:16 +0800

    separate schema class files

---
+++ tests/schema/tests/AuthorSchema.php
@@ -0,0 +1,41 @@
+<?php
+namespace tests;
+use Lazy\Schema\SchemaDeclare;
+use Lazy\Schema;
+
+class AuthorSchema extends SchemaDeclare
+{
+    function schema()
+    {
+        $this->column('id')
+            ->type('integer')
+            ->isa('int')
+            ->primary()
+            ->autoIncrement();
+
+        $this->column('name')
+            ->isa('str')
+            ->varchar(128);
+
+        $this->column('email')
+            ->isa('str')
+            ->required()
+            ->varchar(128);
+
+        $this->column('identity')
+            ->isa('str')
+            ->unique()
+            ->required()
+            ->varchar(128);
+
+        $this->column('confirmed')
+            ->isa('bool')
+            ->default(false)
+            ->boolean();
+
+        $this->mixin('tests\MetadataMixinSchema');
+
+        // $this->belongsTo( '\tests\AuthorBookSchema' , 'author_id' );
+    }
+
+}
