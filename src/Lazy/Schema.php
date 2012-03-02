33912f835abde1c070d746dcaa67a9582b3b0611
c9s <cornelius.howl@gmail.com>
2012-03-02 18:31:20 +0800

    Fix getColumn method

--- src/Lazy/Schema.php
+++ src/Lazy/Schema.php
@@ -55,7 +55,7 @@ class Schema
     {
         $columns = array();
         foreach( $this->columns as $name => $data ) {
-            $columns[] = $this->getColumn( $name );
+            $columns[$name] = $this->getColumn( $name );
         }
         return $columns;
     }
