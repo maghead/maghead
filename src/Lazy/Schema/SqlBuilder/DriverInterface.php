85208598c7bb0e2f99d77c2e88e62b84014840bc
Yo-An Lin <yoanlin93@gmail.com>
2012-03-15 15:23:34 +0800

    Update sqlbuilder interface

--- src/Lazy/Schema/SqlBuilder/DriverInterface.php
+++ src/Lazy/Schema/SqlBuilder/DriverInterface.php
@@ -1,9 +1,10 @@
 <?php
 namespace Lazy\Schema\SqlBuilder;
+use Lazy\Schema\SchemaDeclare;
 
 interface DriverInterface
 {
-	// public function build($schema, $rebuild = false );
+	public function build(SchemaDeclare $schema, $rebuild = false );
 }
 
 
