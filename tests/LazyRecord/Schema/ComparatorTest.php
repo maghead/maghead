6f601424a7123039c7bb68c11eb85fa076f7a985
c9s <cornelius.howl@gmail.com>
2012-04-07 21:50:22 +0800

    Add comparator test

---
+++ tests/LazyRecord/Schema/ComparatorTest.php
@@ -0,0 +1,26 @@
+<?php
+
+class ComparatorTest extends PHPUnit_Framework_TestCase
+{
+    function test()
+    {
+        $a = new LazyRecord\Schema\SchemaDeclare;
+        $a->column('same');
+        $a->column('changed')
+            ->varchar(20);
+        $a->column('removed')
+            ->boolean();
+
+        $b = new LazyRecord\Schema\SchemaDeclare;
+        $b->column('same');
+        $b->column('changed')
+            ->varchar(30);
+        $b->column('added')
+            ->varchar(10);
+
+        $comparator = new LazyRecord\Schema\Comparator;
+        $diff = $comparator->compare( $a , $b );
+        ok( $diff );
+    }
+}
+
