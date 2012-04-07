ece41f5695f103b7bdb8e06dde14023404a04eaf
c9s <cornelius.howl@gmail.com>
2012-04-07 17:26:31 +0800

    Build package.xml and phar file for 1.2.0

---
+++ scripts/release.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+phpunit tests && ( 
+    bash scripts/compile.sh
+    VERSION=$(cat package.ini | grep "^version" | perl -pe 's/version\s*=\s*//i;')
+    onion build
+    git commit -a -m "Build package.xml and phar file for $VERSION"
+    git tag $VERSION -m "Release $VERSION"
+    git push origin --tags
+    sudo pear install -f package.xml
+)
