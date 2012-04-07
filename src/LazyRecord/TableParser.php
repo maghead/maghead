68dc6e4a507809112c1e3865bf436eb35e604e6d
Yo-An Lin <yoanlin93@gmail.com>
2012-04-07 18:29:49 +0800

    Refactor schema base class

--- src/LazyRecord/TableParser.php
+++ src/LazyRecord/TableParser.php
@@ -14,12 +14,19 @@ class MysqlTableParser
 
     public function getTables()
     {
-
+        $stm = $this->connection->query('show tables;');
+        $rows = $stm->fetchAll();
+        return array_map(function($row) { return $row[0]; },$rows);
     }
 
-    public function getTableSpec()
+    public function getTableSchema($table)
     {
+        $stm = $this->connection->query("show columns from $table;");
+        $schema = new Schema\RuntimeSchema;
+        $rows = $stm->fetchAll();
+        foreach( $rows as $row ) {
 
+        }
     }
 }
 
