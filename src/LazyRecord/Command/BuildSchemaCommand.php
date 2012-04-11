aa0b75f2f77b4b557589210eb6d9202a6937b1c3
c9s <cornelius.howl@gmail.com>
2012-04-11 17:03:30 +0800

    Utils class

--- src/LazyRecord/Command/BuildSchemaCommand.php
+++ src/LazyRecord/Command/BuildSchemaCommand.php
@@ -23,46 +23,6 @@ class BuildSchemaCommand extends \CLIFramework\Command
         return 'build configuration file.';
     }
 
-    public function getSchemaClassFromPathsOrClassNames($args,$logger = null)
-    {
-        $classes = array();
-        if( count($args) && ! file_exists($args[0]) ) {
-            // it's classnames
-            foreach( $args as $class ) {
-                // call class loader to load
-                if( class_exists($class,true) ) {
-                    $classes[] = $class;
-                }
-                else {
-                    if( $logger )
-                        $logger->warn( "$class not found." );
-                    else
-                        echo ">>> $class not found.\n";
-                }
-            }
-        }
-        else {
-            $finder = new SchemaFinder;
-            if( count($args) && file_exists($args[0]) ) {
-                $finder->paths = $args;
-            } 
-            // load schema paths from config
-            elseif( $paths = $loader->getSchemaPaths() ) {
-                $finder->paths = $paths;
-            }
-            $finder->loadFiles();
-
-            // load class from class map
-            if( $classMap = $loader->getClassMap() ) {
-                foreach( $classMap as $file => $class ) {
-                    if( ! is_integer($file) && is_string($file) )
-                        require $file;
-                }
-            }
-            $classes = $finder->getSchemaClasses();
-        }
-        return $classes;
-    }
 
     public function execute()
     {
@@ -77,7 +37,7 @@ class BuildSchemaCommand extends \CLIFramework\Command
         $generator->setLogger( $logger );
 
         $args = func_get_args();
-        $classes = $this->getSchemaClassFromPathsOrClassNames( $args , $this->logger );
+        $classes = \LazyRecord\Utils::getSchemaClassFromPathsOrClassNames( $args , $this->logger );
         $classMap = $generator->generate($classes);
 
         $logger->info('Classmap:');
