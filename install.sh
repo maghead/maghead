334ece79a7d08933bf586f5037810aa50c56de89
Yo-An Lin <yoanlin93@gmail.com>
2012-04-22 19:35:52 +0800

    Fix install.sh

--- install.sh
+++ install.sh
@@ -8,28 +8,31 @@ read -p "Install php-sqlite? (Y/n)" install_sqlite
 read -p "Install php-pgsql? (Y/n)" install_pgsql
 read -p "Install php-mysql? (Y/n)" install_mysql
 
+echo "Installing Onion..."
+curl -L -s http://install.onionphp.org/ | bash
+
 if [[ $os == "Darwin" ]] ; then
     port=$(which port)
     brew=$(which brew)
     if [[ -e $port ]] ; then
-        sudo $port install php5-yaml
-        sudo $port install php5-apc
+        sudo $port -q install php5-yaml
+        sudo $port -q install php5-apc
 
-        if [[ $install_mysql != 'n' ]]  ; then sudo $port install php5-mysql      ; fi
-        if [[ $install_sqlite != 'n' ]] ; then sudo $port install php5-sqlite     ; fi
-        if [[ $install_pgsql != 'n' ]]  ; then sudo $port install php5-postgresql ; fi
+        if [[ $install_mysql != 'n' ]]  ; then sudo $port -q install php5-mysql      ; fi
+        if [[ $install_sqlite != 'n' ]] ; then sudo $port -q install php5-sqlite     ; fi
+        if [[ $install_pgsql != 'n' ]]  ; then sudo $port -q install php5-postgresql ; fi
     elif [[ -e $brew ]] ; then
         echo "brew install: not supported yet"
     fi
 elif [[ $os == "Linux" ]] ; then
     apt=$(which apt-get)
     if [[ -e $apt ]] ; then
-        sudo $apt install -q -y php5-dev
-        sudo $apt install -q -y php5-cli
-        sudo $apt install -q -y php-apc
+        sudo $apt install -qq -y php5-dev
+        sudo $apt install -qq -y php5-cli
+        sudo $apt install -qq -y php-apc
         if [[ $install_mysql != 'n' ]]  ; then sudo $apt install -qq -y php5-mysql      ; fi
         if [[ $install_sqlite != 'n' ]] ; then sudo $apt install -qq -y php5-sqlite     ; fi
-        if [[ $install_pgsql != 'n' ]]  ; then sudo $apt install -qq -y php5-pgsql ; fi
+        if [[ $install_pgsql != 'n' ]]  ; then sudo $apt install -qq -y php5-pgsql      ; fi
     fi
 fi
 
