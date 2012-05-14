6c82f01bac98c33da5682e9185990f3556f11710
c9s <cornelius.howl@gmail.com>
2012-05-14 09:29:07 +0800

    update install.sh script

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 onion build
 for p in $(which -a pear) ; do
-    sudo $p channel-discover pear.corneltek.com
-    sudo $p install -f package.xml
+    $p channel-discover pear.corneltek.com
+    echo Installing for $p ...
+    $p -v install -f package.xml
 done
