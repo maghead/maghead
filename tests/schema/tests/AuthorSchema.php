1512f9667c2e21a4e7607d5f5e5d3651ea923d19
c9s <cornelius.howl@gmail.com>
2012-05-22 16:54:38 +0800

    Support Apache-Ant

--- tests/schema/tests/AuthorSchema.php
+++ tests/schema/tests/AuthorSchema.php
@@ -9,24 +9,20 @@ class AuthorSchema extends SchemaDeclare
     {
 
         $this->column('name')
-            ->isa('str')
             ->varchar(128);
 
         $this->column('email')
-            ->isa('str')
             ->required()
             ->varchar(128);
 
         $this->column('identity')
-            ->isa('str')
             ->unique()
             ->required()
             ->varchar(128);
 
         $this->column('confirmed')
-            ->isa('bool')
-            ->default(false)
-            ->boolean();
+            ->boolean()
+            ->default(false);
 
         $this->mixin('LazyRecord\Schema\Mixin\MetadataSchema');
 
