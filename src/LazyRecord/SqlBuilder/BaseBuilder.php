d743cd4f07011746b5d3b7eb203c474a2817fc5a
Yo-An Lin <yoanlin93@gmail.com>
2012-07-03 01:21:03 +0800

    Extract createTable method to BaseBuilder

--- src/LazyRecord/SqlBuilder/BaseBuilder.php
+++ src/LazyRecord/SqlBuilder/BaseBuilder.php
@@ -18,6 +18,22 @@ class BaseBuilder
         }
     }
 
+
+    public function createTable($schema)
+    {
+        $sql = 'CREATE TABLE ' 
+            . $this->driver->getQuoteTableName($schema->getTable()) . " ( \n";
+        $columnSql = array();
+        foreach( $schema->columns as $name => $column ) {
+            if( $column->virtual )
+                continue;
+            $columnSql[] = '  ' . $this->buildColumnSql( $schema, $column );
+        }
+        $sql .= join(",\n",$columnSql);
+        $sql .= "\n);\n";
+        return $sql;
+    }
+
     public function __get($name)
     {
         return $this->driver->$name;
