befce80bab8a6dae673cff1723e2c81c57d406f1
Yo-An Lin <yoanlin93@gmail.com>
2012-07-03 01:22:20 +0800

    Add createTable, dropTable method to interface

--- src/LazyRecord/SqlBuilder/BuilderInterface.php
+++ src/LazyRecord/SqlBuilder/BuilderInterface.php
@@ -4,8 +4,11 @@ use LazyRecord\Schema\SchemaDeclare;
 
 interface BuilderInterface
 {
-	public function build($schema);
+    public function build($schema);
     public function buildIndex($schema);
+
+    public function createTable($schema);
+    public function dropTable($schema);
 }
 
 
