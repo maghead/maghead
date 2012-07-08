9fdae5a1e851ef7d2ecc940d3b365a650daf1a08
Yo-An Lin <yoanlin93@gmail.com>
2012-07-09 00:28:49 +0800

    implement build-basedata command

--- src/LazyRecord/Command/BuildBaseDataCommand.php
+++ src/LazyRecord/Command/BuildBaseDataCommand.php
@@ -12,6 +12,24 @@ class BuildBaseDataCommand extends Command
     function brief() { return 'insert basedata into datasource.'; }
 
 
+    public function execute()
+    {
+        $options = $this->options;
+        $logger  = $this->logger;
+
+        CommandUtils::set_logger($this->logger);
+        CommandUtils::init_config_loader();
+
+        $classes = CommandUtils::find_schemas_with_arguments( func_get_args() );
+
+        CommandUtils::print_schema_classes($classes);
+
+        $schemas = CommandUtils::schema_classes_to_objects( $classes );
+
+        CommandUtils::build_basedata($schemas);
+
+        $this->logger->info('Done');
+    }
 }
 
 
