0537be67eeb6615efdec20ac240e8949eb15cbdd
Yo-An Lin <yoanlin93@gmail.com>
2012-07-09 12:17:34 +0800

    doc string for schema loader

--- src/LazyRecord/Schema/SchemaLoader.php
+++ src/LazyRecord/Schema/SchemaLoader.php
@@ -1,19 +1,28 @@
 <?php
 namespace LazyRecord\Schema;
 
+
+/**
+ * Schema loader actually catches 
+ * schema object instances by schema class name.
+ */
 class SchemaLoader
 {
     static $schemas = array();
 
+
+    /**
+     * Load or create schema object.
+     *
+     * @param string $class Schema class naem
+     *
+     * @return LazyRecord\Schema\RuntimeSchema
+     */
     static function load($class)
     {
         return ( isset($schemas[ $class ] ) ) 
                 ? $schemas[ $class ] 
                 : $schemas[ $class ] = new $class;
     }
-
 }
-
-
-
 
