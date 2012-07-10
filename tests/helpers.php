92d3fc5943902610c601dbfa9507462ea0cbdcfa
Yo-An Lin <yoanlin93@gmail.com>
2012-07-10 11:51:21 +0800

    Add PHPUnit TestHelper

---
+++ tests/helpers.php
@@ -0,0 +1,17 @@
+<?php
+
+function result_ok( $result )
+{
+    $stacks = debug_backtrace( DEBUG_BACKTRACE_PROVIDE_OBJECT ); 
+    $tester = $stacks[1]['object'];
+    $tester->assertTrue( $result->success, $result->message );
+    return $result->success;
+}
+
+function result_fail( $result ) {
+    $stacks = debug_backtrace( DEBUG_BACKTRACE_PROVIDE_OBJECT ); 
+    $tester = $stacks[1]['object'];
+    $tester->assertFalse( $result->success, $result->message );
+    return ! $result->success;
+}
+
