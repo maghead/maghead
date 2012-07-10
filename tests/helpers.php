1b89a2fc1fc06c177373042c372c972e78377e78
Yo-An Lin <yoanlin93@gmail.com>
2012-07-10 12:27:33 +0800

    Use more result_ok

--- tests/helpers.php
+++ tests/helpers.php
@@ -4,6 +4,7 @@ function result_ok( $result )
 {
     $stacks = debug_backtrace( DEBUG_BACKTRACE_PROVIDE_OBJECT ); 
     $tester = $stacks[1]['object'];
+    $tester->assertTrue( $result ? true : false );
     $tester->assertTrue( $result->success, $result->message );
     return $result;
 }
@@ -11,6 +12,7 @@ function result_ok( $result )
 function result_fail( $result ) {
     $stacks = debug_backtrace( DEBUG_BACKTRACE_PROVIDE_OBJECT ); 
     $tester = $stacks[1]['object'];
+    $tester->assertTrue( $result ? true : false );
     $tester->assertFalse( $result->success, $result->message );
     return $result;
 }
