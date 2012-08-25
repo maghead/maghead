1ae197a15501be283ee15e55219a38d23669727b
Yo-An Lin <yoanlin93@gmail.com>
2012-08-25 18:42:03 +0800

    Check extension for testing

--- tests/LazyRecord/MysqlModelTest.php
+++ tests/LazyRecord/MysqlModelTest.php
@@ -5,5 +5,12 @@ class MysqlModelTest extends ModelTest
 {
     public $driver = 'mysql';
     public $schemaPath = 'tests/schema';
+    public function setUp() {
+        if( ! extension_loaded('mysql') )
+            $this->markTestSkipped('mysql extension is required for testing');
+        if( ! extension_loaded('pdo_mysql') )
+            $this->markTestSkipped('pdo_mysql extension is required for testing');
+        return parent::setUp();
+    }
 }
 
