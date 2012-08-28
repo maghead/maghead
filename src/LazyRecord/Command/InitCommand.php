1d61d9a4747ef56c7cb6cfd09e307b859a8c9e29
Yo-An Lin <yoanlin93@gmail.com>
2012-08-28 17:56:35 +0800

    Add init command to initialize the structure

---
+++ src/LazyRecord/Command/InitCommand.php
@@ -0,0 +1,32 @@
+<?php
+namespace LazyRecord\Command;
+use Exception;
+use CLIFramework\Command;
+
+class InitCommand extends Command 
+{
+    public function brief() {
+        return 'initialize your lazyrecord structures.';
+    }
+
+    public function mkpath($path) {
+        if( ! file_exists($path) ) {
+            $this->logger->info($path);
+            mkdir($path,0755,true);
+        }
+    }
+
+    public function execute()
+    {
+        $this->mkpath('db/config');
+        $this->mkpath('db/migrations');
+
+        $command = new InitConfCommand;
+        $command->application = $this->application;
+        $command->execute();
+
+        $command = new BuildConfCommand;
+        $command->application = $this->application;
+        $command->execute('db/config/database.yml');
+    }
+}
