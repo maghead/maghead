ea38a0a1e355e9b36814f1147b95e7f69c34d1e2
c9s <cornelius.howl@gmail.com>
2012-08-28 23:57:25 +0800

    method name ordering

--- src/LazyRecord/Schema/SchemaDataInterface.php
+++ src/LazyRecord/Schema/SchemaDataInterface.php
@@ -3,17 +3,28 @@ namespace LazyRecord\Schema;
 
 interface SchemaDataInterface 
 {
-    public function getModelClass();
+
     public function getTable();
+
     public function getLabel();
-    public function getRelation($relationId);
-    public function getRelations();
+
     public function getReferenceSchemas($recursive = true);
+
     public function getColumns();
+
     public function getColumn($name);
+
     public function hasColumn($name);
+
+    public function getModelClass();
+
     public function getReadSourceId();
+
     public function getWriteSourceId();
+
+    public function getRelation($relationId);
+
+    public function getRelations();
 }
 
 
