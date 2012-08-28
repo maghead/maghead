c7ca3303eb7b92dd28c019cb09b62f84e9a41b1e
Yo-An Lin <yoanlin93@gmail.com>
2012-08-28 23:59:24 +0800

    Fix ci

---
+++ db/config/database.php
@@ -0,0 +1,16 @@
+<?php return array (
+  'bootstrap' => 
+  array (
+    0 => 'tests/bootstrap.php',
+  ),
+  'schema' => NULL,
+  'data_sources' => 
+  array (
+    'default' => 
+    array (
+      'dsn' => 'sqlite::memory:',
+      'user' => NULL,
+      'pass' => NULL,
+    ),
+  ),
+);
