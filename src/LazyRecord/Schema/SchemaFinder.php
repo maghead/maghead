1a8cf8b0e7a733728707c5ab7f758318bc0c38f1
c9s <cornelius.howl@gmail.com>
2012-08-28 20:05:53 +0800

    schema finder steps

--- src/LazyRecord/Schema/SchemaFinder.php
+++ src/LazyRecord/Schema/SchemaFinder.php
@@ -12,6 +12,8 @@ use IteratorAggregate;
 /**
  * Find schema classes from files (or from current runtime)
  *
+ * 1. Find SchemaDeclare-based schema class files.
+ * 2. Find model-based schema, pass dynamic schema class 
  */
 class SchemaFinder
     implements IteratorAggregate
