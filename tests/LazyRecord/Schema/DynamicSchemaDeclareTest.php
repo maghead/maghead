cd090f0d82934a7b1347b79f45f33e51e910e71d
c9s <cornelius.howl@gmail.com>
2012-08-29 10:24:13 +0800

    More schema tests

--- tests/LazyRecord/Schema/DynamicSchemaDeclareTest.php
+++ tests/LazyRecord/Schema/DynamicSchemaDeclareTest.php
@@ -2,13 +2,48 @@
 
 class DynamicSchemaDeclareTest extends PHPUnit_Framework_TestCase
 {
-    function test()
+    function testSchema()
     {
         $wine = new \tests\Wine;
         ok($wine);
-
         $schema = new LazyRecord\Schema\DynamicSchemaDeclare( $wine );
-
+        ok($schema);
+        return $schema;
     }
+
+
+    /**
+     * @depends testSchema
+     */
+    function testTable($schema)
+    {
+        is('wines',$schema->getTable());
+    }
+
+    /**
+     * @depends testSchema
+     */
+    function testModelClass($schema)
+    {
+        is('tests\Wine',$schema->getModelClass());
+    }
+
+    /**
+     * @depends testSchema
+     */
+    function testGetNamespace($schema)
+    {
+        is('tests',$schema->getNamespace());
+    }
+
+    /**
+     * @depends testSchema
+     */
+    function testColumns($schema)
+    {
+        ok( $schema->getColumn('name') );
+        ok( $schema->getColumn('years') );
+    }
+
 }
 
