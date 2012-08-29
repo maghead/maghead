c48391411c5253ee07acf92adb23e87541a4356a
Yo-An Lin <yoanlin93@gmail.com>
2012-08-29 22:01:14 +0800

    Use class injector to inject class contents

--- tests/schema/tests/Wine.php
+++ tests/schema/tests/Wine.php
@@ -12,4 +12,10 @@ class Wine extends BaseModel
         $schema->column('years')
             ->integer();
     }
+#boundary start 7c7b528eb73d02172dcec82d792e1699
+const schema_proxy_class = 'tests\\WineSchemaProxy';
+const collection_class = 'tests\\WineCollection';
+const model_class = 'tests\\Wine';
+const table = 'wines';
+#boundary end 7c7b528eb73d02172dcec82d792e1699
 }
