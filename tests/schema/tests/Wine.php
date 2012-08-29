53302905c8ab4809ed9f3a912b64ee87d1d33bf8
c9s <cornelius.howl@gmail.com>
2012-08-29 10:28:38 +0800

    Wine model for testing

---
+++ tests/schema/tests/Wine.php
@@ -0,0 +1,18 @@
+<?php
+namespace tests;
+use LazyRecord\BaseModel;
+
+class Wine extends BaseModel
+{
+
+    function schema($schema)
+    {
+        $schema->column('name')
+            ->varchar(128);
+
+        $schema->column('years')
+            ->integer();
+    }
+
+}
+
