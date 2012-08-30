a053ac2ada21d56af55496987186cfa89d8cce6d
Yo-An Lin <yoanlin93@gmail.com>
2012-08-31 00:42:05 +0800

    basic method call rendering

--- src/LazyRecord/CodeGen/MethodCall.php
+++ src/LazyRecord/CodeGen/MethodCall.php
@@ -19,10 +19,30 @@ class MethodCall
         return $this;
     }
 
-    function pushArgument($arg) 
+    function addArgument($arg) 
     {
         $this->arguments[] = $arg;
         return $this;
+    }
+
+    function render() {
+        $code = '';
+        $code .= '$' . $this->objectName;
+        $code .= '->' . $this->method . '(';
+
+        $strs = array();
+        foreach( $this->arguments as $arg ) {
+            $str = var_export($arg,true);
+            $strs[] = $str;
+        }
+        $code .= join(',',$strs);
+        $code .= ')';
+        return $code;
+    }
+
+    function __toString() 
+    {
+        return $this->render();
     }
 }
 
