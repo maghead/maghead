b429f5fdd2da7ce4eb2e2dbe3455dffca47350a7
Yo-An Lin <yoanlin93@gmail.com>
2012-08-30 16:01:39 +0800

    Migration

---
+++ src/LazyRecord/Migration/MigrationGenerator.php
@@ -0,0 +1,42 @@
+<?php
+namespace LazyRecord\Migration;
+use Exception;
+use ReflectionClass;
+use ReflectionObject;
+use CLIFramework\Command;
+use LazyRecord\Schema;
+use LazyRecord\Schema\SchemaFinder;
+use LazyRecord\ConfigLoader;
+use LazyRecord\TableParser;
+
+class MigrationGenerator
+{
+    public $driver;
+    public $connection;
+    public $migrationDir;
+
+    function __construct($dataSourceId,$migrationDir) 
+    {
+        $connectionManager = \LazyRecord\ConnectionManager::getInstance();
+        $this->connection  = $connectionManager->getConnection($id);
+        $this->driver      = $connectionManager->getQueryDriver($id);
+        $this->migrationDir = $migrationDir;
+    }
+
+
+    function generate()
+    {
+        $parser = TableParser::create( $this->driver, $this->connection );
+        $tableSchemas = array();
+        $tables = $parser->getTables();
+        foreach(  $tables as $table ) {
+            $tableSchemas[ $table ] = $parser->getTableSchema( $table );
+        }
+
+        $finder = new SchemaFinder;
+        $finder->loadFiles();
+        $schemas = $finder->getSchemas();
+    }
+}
+
+
