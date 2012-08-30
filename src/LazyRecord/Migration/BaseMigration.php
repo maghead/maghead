d728ec75403b486ab32ca0b96eef7e0b30c332d0
c9s <cornelius.howl@gmail.com>
2012-08-31 00:47:12 +0800

    Implement BaseMigration class

--- src/LazyRecord/Migration/BaseMigration.php
+++ src/LazyRecord/Migration/BaseMigration.php
@@ -4,7 +4,20 @@ use SQLBuilder\MigrationBuilder;
 
 class BaseMigration
 {
+    public $driver;
+    public $builder;
 
+    public function __construct($driver) 
+    {
+        $this->driver = $driver;
+        $this->builder = new MigrationBuilder($driver);
+    }
+
+
+
+    function __call($m,$a) {
+        return call_user_func_array( array($this->builder,$m) , $a );
+    }
 
 
 }
