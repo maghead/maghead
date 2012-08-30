6228878859736dc2fd40303c3531c487846f5691
c9s <cornelius.howl@gmail.com>
2012-08-30 16:01:39 +0800

    Migration

---
+++ tests/LazyRecord/Migration/MigrationGeneratorTest.php
@@ -0,0 +1,10 @@
+<?php
+
+class MigrationGeneratorTest extends PHPUnit_Framework_TestCase
+{
+    function test()
+    {
+        $generator = new LazyRecord\Migration\MigrationGenerator;
+    }
+}
+
