d06efac43e4041316a09d586e2c7c9cbeb3bc159
c9s <cornelius.howl@gmail.com>
2012-08-31 01:50:05 +0800

    BaseMigrationTest: remove data source.

--- tests/LazyRecord/Migration/BaseMigrationTest.php
+++ tests/LazyRecord/Migration/BaseMigrationTest.php
@@ -2,19 +2,24 @@
 
 class FooMigration extends LazyRecord\Migration\BaseMigration 
 {
-
     public function upgrade() 
     {
     }
-
-
 }
 
 class BaseMigrationTest extends PHPUnit_Framework_TestCase
 {
     function test()
     {
-        
+        $connm = LazyRecord\ConnectionManager::getInstance();
+        $connm->addDataSource('default',array(
+            'dsn' => 'sqlite::memory:'
+        ));
+
+        $migration = new FooMigration('default');
+        ok($migration);
+
+        $connm->removeDataSource('default');
     }
 }
 
