76b375801dc1eeaac0de6ce823815ffbe7a699f4
c9s <cornelius.howl@gmail.com>
2012-09-01 16:37:59 +0800

    More class generator test

--- tests/LazyRecord/CodeGen/ClassTemplateTest.php
+++ tests/LazyRecord/CodeGen/ClassTemplateTest.php
@@ -16,13 +16,21 @@ class ClassTemplateTest extends PHPUnit_Framework_TestCase
         ));
         ok($class1);
 
+        $class1->addMethod('public','getTwo',array(),'return 2;');
+        $class1->addMethod('public','getFoo',array('i'),'return $i;');
         $code = $class1->render();
-
         $tmpname = tempnam('/tmp','FOO');
         file_put_contents($tmpname, $code);
         require $tmpname;
 
         ok(class_exists('Foo\Bar22'));
+
+        $bar22 = new Foo\Bar22;
+        ok($bar22);
+
+        is(2,$bar22->getTwo());
+
+        is(3,$bar22->getFoo(3));
 
         unlink($tmpname);
     }
