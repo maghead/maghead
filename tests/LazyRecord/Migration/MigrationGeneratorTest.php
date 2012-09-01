81a55ddf2c43cf7a8ffbb423756938d11e44becb
Yo-An Lin <yoanlin93@gmail.com>
2012-09-01 17:53:48 +0800

    Add diff migration generator test

--- tests/LazyRecord/Migration/MigrationGeneratorTest.php
+++ tests/LazyRecord/Migration/MigrationGeneratorTest.php
@@ -2,13 +2,42 @@
 
 class MigrationGeneratorTest extends PHPUnit_Framework_TestCase
 {
-    function test()
+
+    function setUp()
     {
         $connectionManager = \LazyRecord\ConnectionManager::getInstance();
-        $connectionManager->addDataSource('default',array(
-            'dsn' => 'sqlite::memory:'
-        ));
+        $connectionManager->addDataSource('default',array( 'dsn' => 'sqlite::memory:' ));
+    }
 
+    function tearDown()
+    {
+        $connectionManager = \LazyRecord\ConnectionManager::getInstance();
+        $connectionManager->removeDataSource('default');
+        $connectionManager->close('default');
+    }
+
+
+    function testGenerator()
+    {
+        $connectionManager = \LazyRecord\ConnectionManager::getInstance();
+        $pdo = $connectionManager->getConnection('default');
+        $pdo->query('create table users (id integer not null primary key);');
+
+        $generator = new \LazyRecord\Migration\MigrationGenerator('default','tests/migrations');
+        ok($generator);
+
+        is('20120901_CreateUser.php',$generator->generateFilename('CreateUser'),'20120901');
+
+        list($class,$path) = $generator->generate('UpdateUser','20120902');
+        is('UpdateUser_1346515200', $class);
+        path_ok($path);
+        is('tests/migrations/20120902_UpdateUser.php',$path);
+        unlink($path);
+    }
+
+    function testDiffMigration() 
+    {
+        $connectionManager = \LazyRecord\ConnectionManager::getInstance();
         $pdo = $connectionManager->getConnection('default');
         $pdo->query('create table users (id integer not null primary key);');
 
@@ -27,10 +56,7 @@ class MigrationGeneratorTest extends PHPUnit_Framework_TestCase
 
         $finder = new \LazyRecord\Schema\SchemaFinder;
         $finder->find();
-
         # $generator->generateWithDiff('TaskName',$finder->getSchemas() );
-
-        $connectionManager->removeDataSource('default');
     }
 }
 
