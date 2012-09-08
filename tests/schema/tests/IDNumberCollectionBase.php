50fb2d70181215f7d9493ea96636ccc14fe8d40a
Yo-An Lin <yoanlin93@gmail.com>
2012-09-08 16:32:25 +0800

    Add IDNumber Model

---
+++ tests/schema/tests/IDNumberCollectionBase.php
@@ -0,0 +1,9 @@
+<?php
+namespace tests;
+
+class IDNumberCollectionBase  extends \LazyRecord\BaseCollection {
+const schema_proxy_class = '\\tests\\IDNumberSchemaProxy';
+const model_class = '\\tests\\IDNumber';
+const table = 'i_d_numbers';
+
+}
