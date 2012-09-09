0594017e0c58a531d803125bc7cc46256561afc3
c9s <cornelius.howl@gmail.com>
2012-09-09 14:29:39 +0800

    improve printing and move some messages to debug level.

--- src/LazyRecord/Command/BuildSchemaCommand.php
+++ src/LazyRecord/Command/BuildSchemaCommand.php
@@ -39,12 +39,11 @@ class BuildSchemaCommand extends \CLIFramework\Command
         $generator->setLogger( $logger );
         $classMap = $generator->generate($classes);
 
-        $logger->info('Classmap:');
         foreach( $classMap as $class => $file ) {
             $path = $file;
             if( strpos( $path , getcwd() ) === 0 )
                 $path = substr( $path , strlen(getcwd()) + 1 );
-            $logger->info(sprintf("%-32s",$class) . " => $path",1);
+            $logger->info(sprintf("%-32s",ltrim($class,'\\')) . " => $path",1);
         }
         $logger->info('Done');
     }
