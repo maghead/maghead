d02b276bf73f76c1fb007d08d56398f01b509d41
Yo-An Lin <yoanlin93@gmail.com>
2012-09-10 11:01:39 +0800

    Print validation results

--- src/LazyRecord/OperationResult/OperationError.php
+++ src/LazyRecord/OperationResult/OperationError.php
@@ -4,7 +4,9 @@ namespace LazyRecord\OperationResult;
 class OperationError extends OperationResult
 {
     public $success = false;
+
     public $exception;
+
     public $code;
 
     public function __toString()
@@ -21,6 +23,18 @@ class OperationError extends OperationResult
             $str .= "\nSQL: " . $this->sql;
         if( $this->vars )
             $str .= "\nVars: " . print_r($this->vars,true);
+        if( $this->validations ) {
+            $str .= "\nValidations: ";
+            foreach( $this->validations as $v ) {
+                $str .= "\n" . (
+                    is_array($v) 
+                        ? print_r($v,true) 
+                        : (is_object($v) 
+                            ? $v->__toString() 
+                            : $v)
+                );
+            }
+        }
         return $str;
     }
 
