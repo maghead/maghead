bd00c5ded7200fa49c16dffd6b8df0084f6c4b6f
c9s <cornelius.howl@gmail.com>
2012-09-10 13:06:05 +0800

    rebuild package

--- src/LazyRecord/Console.php
+++ src/LazyRecord/Console.php
@@ -5,7 +5,7 @@ use CLIFramework\Application;
 class Console extends Application
 {
     const name = 'LazyRecord';
-    const version = '1.8.5';
+    const version = '1.8.6';
 
     public function brief()
     {
