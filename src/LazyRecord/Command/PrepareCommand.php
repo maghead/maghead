abfad630ccf42f92139692424b6655f313ef66d8
Yo-An Lin <yoanlin93@gmail.com>
2012-09-13 00:33:24 +0800

    Let prepare command be inherted from ChainedCommand class.

--- src/LazyRecord/Command/PrepareCommand.php
+++ src/LazyRecord/Command/PrepareCommand.php
@@ -1,45 +1,20 @@
 <?php
 namespace LazyRecord\Command;
-use CLIFramework\Command;
+use CLIFramework\ChainedCommand;
 use LazyRecord\Command\BuildSchemaCommand;
 use LazyRecord\Command\BuildSqlCommand;
 use LazyRecord\Command\BuildBasedataCommand;
 
-class PrepareCommand extends Command
+class PrepareCommand extends ChainedCommand
 {
+    public $commands = array(
+        'LazyRecord\\Command\\BuildSchemaCommand',
+        'LazyRecord\\Command\\BuildSqlCommand',
+        'LazyRecord\\Command\\BuildBasedataCommand',
+    );
+
+    public function usage() { return 'lazy prepare'; }
 
     public function brief() { return 'prepare schema and database.'; }
-
-    public function options($opts) 
-    {
-        $cmd1 = new BuildSchemaCommand;
-        $cmd2 = new BuildSqlCommand;
-        $cmd3 = new BuildBasedataCommand;
-        $cmd1->options($opts);
-        $cmd2->options($opts);
-        $cmd3->options($opts);
-    }
-
-    public function execute() 
-    {
-        $args = func_get_args();
-        $app = $this->application;
-        $cmd1 = new BuildSchemaCommand($app);
-        $cmd2 = new BuildSqlCommand($app);
-        $cmd3 = new BuildBasedataCommand($app);
-
-        $logger = $this->getLogger();
-        $options = $this->getOptions();
-        $cmd1->options = $options;
-        $cmd1->logger = $logger;
-        $cmd2->options = $options;
-        $cmd2->logger = $logger;
-        $cmd3->options = $options;
-        $cmd3->logger = $logger;
-
-        $cmd1->executeWrapper($args);
-        $cmd2->executeWrapper($args);
-        $cmd3->executeWrapper($args);
-    }
 }
 
