5dd777696c31af290bcd889e9d020cc567ee9de9
c9s <cornelius.howl@gmail.com>
2013-04-06 23:28:12 +0800

    refactro ArrayUtilsTest

--- tests/LazyRecord/ArrayUtilsTest.php
+++ tests/LazyRecord/ArrayUtilsTest.php
@@ -2,29 +2,42 @@
 
 class ArrayUtilsTest extends PHPUnit_Framework_TestCase
 {
-
-    function arrayProvider()
+    public function testAssocArrayCheck()
     {
-        return array(
-            array(array(  
-                'a' => 'b',
-                '0' => '1',
-            )),
-            array(array(
-                'foo' => 1,
-                'bar' => 2,
-            )),
+
+        $a = array(
+            'a' => 'b',
+            '0' => '1',
         );
+        ok( LazyRecord\ArrayUtils::is_assoc_array($a) );
+
+        $a = array( 
+            'foo' => 'b',
+            'bar' => '1',
+        );
+        ok( LazyRecord\ArrayUtils::is_assoc_array($a) );
     }
 
-
-    /**
-     * @dataProvider arrayProvider
-     */
-    function test($array)
+    public function testIndexedArrayCheck()
     {
-        ok( LazyRecord\ArrayUtils::is_assoc_array( $array ) );
-        ok( ! LazyRecord\ArrayUtils::is_indexed_array( $array ) );
+        $a = array( 
+            0 => 'foo',
+            1 => 'bar',
+        );
+        ok( LazyRecord\ArrayUtils::is_indexed_array($a));
+
+        $a = array( 
+            'a' => 'foo',
+            1 => 'bar',
+        );
+        not_ok( LazyRecord\ArrayUtils::is_indexed_array( $a ));
+
+
+        $a = array(
+            'a' => 'foo',
+            1 => 'bar',
+        );
+        not_ok( LazyRecord\ArrayUtils::is_indexed_array($a));
     }
 }
 
