6a8673d572b55ba43b97ac424480e28e1b32e97c
Yo-An Lin <yoanlin93@gmail.com>
2013-04-07 01:07:00 +0800

    Improve column operation performance

--- src/LazyRecord/Model/MetadataSchemaProxy.php
+++ src/LazyRecord/Model/MetadataSchemaProxy.php
@@ -1,10 +1,35 @@
 <?php
 namespace LazyRecord\Model;
 
+use LazyRecord;
 use LazyRecord\Schema\RuntimeSchema;
+use LazyRecord\Schema\Relationship;
 
 class MetadataSchemaProxy extends RuntimeSchema
 {
+
+    public static $column_names = array (
+  0 => 'id',
+  1 => 'name',
+  2 => 'value',
+);
+    public static $column_hash = array (
+  'id' => 1,
+  'name' => 1,
+  'value' => 1,
+);
+    public static $column_names_include_virtual = array (
+  0 => 'id',
+  1 => 'name',
+  2 => 'value',
+);
+
+    const schema_class = 'LazyRecord\\Schema\\DynamicSchemaDeclare';
+    const collection_class = 'LazyRecord\\Model\\MetadataCollection';
+    const model_class = 'LazyRecord\\Model\\Metadata';
+    const primary_key = 'id';
+    const table = '__meta__';
+    const label = 'Metadata';
 
     public function __construct()
     {
