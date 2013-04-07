ceda0f2c8d321d3ea55cc9e8e2f6ebb35ec0b5a0
Yo-An Lin <yoanlin93@gmail.com>
2013-04-07 17:19:07 +0800

    Checking in changes prior to tagging of version 1.9.22.

--- src/LazyRecord/Console.php
+++ src/LazyRecord/Console.php
@@ -5,7 +5,7 @@ use CLIFramework\Application;
 class Console extends Application
 {
     const name = 'LazyRecord';
-    const VERSION = "1.9.21";
+    const VERSION = "1.9.22";
 
     public function brief()
     {
