44a4d5451a415bc76c6d2e2442d3af2bafd5f03c
Yo-An Lin <yoanlin93@gmail.com>
2013-08-23 22:18:07 +0800

    Checking in changes prior to tagging of version 1.11.13.

--- src/LazyRecord/Console.php
+++ src/LazyRecord/Console.php
@@ -5,7 +5,7 @@ use CLIFramework\Application;
 class Console extends Application
 {
     const name = 'LazyRecord';
-    const VERSION = "1.11.12";
+    const VERSION = "1.11.13";
 
     public function brief()
     {
