5ff4e939957178d870770c59f50f867c3f018fa3
c9s <yoanlin93@gmail.com>
2014-04-11 16:16:10 +0800

    improve mixin methods on BaseModel...

--- src/LazyRecord/Schema/MixinSchemaDeclare.php
+++ src/LazyRecord/Schema/MixinSchemaDeclare.php
@@ -13,6 +13,15 @@ class MixinSchemaDeclare extends SchemaDeclare
     }
 
     /* is_a can not work on an abstract class */
-    public function schema() {
-    }
+    public function schema() { }
+
+
+    /**
+     * compatible methods for BaseModel to mixin
+     */
+    public static function beforeCreate($args) { return $args; }
+    public static function beforeUpdate($args) { return $args; }
+
+    public static function afterCreate($args) {}
+    public static function afterUpdate($args) {}
 }
