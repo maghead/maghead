64981998f2e7dd5ccf75aa30c1aba110131b0e7f
c9s <yoanlin93@gmail.com>
2014-04-20 17:18:08 +0800

    More refacoring

--- src/LazyRecord/ExecutiveQueryBuilder.php
+++ src/LazyRecord/ExecutiveQueryBuilder.php
@@ -1,25 +1,15 @@
 <?php
 namespace LazyRecord;
-
+use Exception;
+use SQLBuilder\SQLBuilder;
 
 class ExecutiveQueryBuilder extends \SQLBuilder\QueryBuilder
 {
     public $callback;
 
-    public function execute()
-    {
+    public function execute() {
         $sql = $this->build();
-        switch( $this->behavior ) {
-            case static::INSERT:
-            case static::UPDATE:
-            case static::DELETE:
-            case static::SELECT:
-                return call_user_func( $this->callback, $this, $sql);
-                break;
-            default:
-                throw new \Exception('behavior is not defined.');
-                break;
-        }
+        return call_user_func( $this->callback, $this, $sql);
     }
 }
 
