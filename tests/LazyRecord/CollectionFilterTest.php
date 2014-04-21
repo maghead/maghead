f0fe2176226c251849c4e4b28183790d3e23ff48
c9s <yoanlin93@gmail.com>
2014-04-22 01:33:31 +0800

    Add CollectionFilterTest

---
+++ tests/LazyRecord/CollectionFilterTest.php
@@ -0,0 +1,28 @@
+<?php
+use LazyRecord\CollectionFilter;
+
+class CollectionFilterTest extends PHPUnit_Framework_TestCase
+{
+    public function test()
+    {
+        /*
+        $posts = new PostCollection;
+        $filter = new CollectionFilter($posts);
+
+        // set up valid status
+        $filter->defineEqual('status', [ 'published', 'draft' ]);
+        $filter->defineContains('content');
+        $filter->defineContains('content');
+        $filter->defineStartWith('content');
+        $filter->defineEndWith('content');
+
+        $filter->defineInset('member_id');
+        $filter->defineInset('member_id', [1,2,3,4]);
+
+        $filter->defineEqual('category_id', CollectionFilter::Integer ); // must be a integer type
+
+        $filter->defineEqual('is_deleted', CollectionFilter::Boolean ); // must be a integer type
+        */
+    }
+}
+
