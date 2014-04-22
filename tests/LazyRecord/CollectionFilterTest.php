455a1966d4a8b872ab5ea27e62c8861f784d2008
c9s <yoanlin93@gmail.com>
2014-04-22 10:59:02 +0800

    Basic workable collection filter

--- tests/LazyRecord/CollectionFilterTest.php
+++ tests/LazyRecord/CollectionFilterTest.php
@@ -1,16 +1,48 @@
 <?php
 use LazyRecord\CollectionFilter;
+use tests\PostCollection;
+use tests\Post;
 
+/**
+ * Generate SQL statement like this:
+ *
+ * SELECT m.title, m.content, m.status, m.id FROM posts m  WHERE status = published AND status = published1 AND content like %foo% AND content like %foo%1 AND created_on BETWEEN '2011-01-01' AND '2011-12-30'
+ *
+ */
 class CollectionFilterTest extends PHPUnit_Framework_TestCase
 {
     public function test()
     {
         /*
+        $post = new Post;
+        $ret = $post->create([ 
+            'title' => 'title content',
+            'content' => 'foo bar',
+            'status' => 'published',
+        ]);
+        ok($post->id, $ret);
+        */
         $posts = new PostCollection;
         $filter = new CollectionFilter($posts);
+        ok($filter);
 
-        // set up valid status
         $filter->defineEqual('status', [ 'published', 'draft' ]);
+        $filter->defineContains('content');
+        $filter->defineRange('created_on', CollectionFilter::String );
+
+        $collection = $filter->apply([ 
+            'status' => ['published','draft'],
+            'content' => ['foo', 'bar'],
+            'created_on' => [ '2011-01-01', '2011-12-30' ],
+        ]);
+        ok( $collection );
+
+        echo $collection->toSql();
+
+
+
+        /*
+        // set up valid status
         $filter->defineContains('content');
         $filter->defineContains('content');
         $filter->defineStartWith('content');
