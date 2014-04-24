9a17e71af504db05a03e1a3562555a1466681ea9
c9s <yoanlin93@gmail.com>
2014-04-24 18:33:47 +0800

    Migrate phifty model methods to BaseModel

--- src/LazyRecord/BaseModel.php
+++ src/LazyRecord/BaseModel.php
@@ -26,6 +26,7 @@ use SerializerKit\YamlSerializer;
 
 use ValidationKit\ValidationMessage;
 use ActionKit;
+use ActionKit\RecordAction\BaseRecordAction;
 
 /**
  * Base Model class,
@@ -2298,6 +2299,41 @@ abstract class BaseModel implements
         }
     }
 
+
+    public function asCreateAction($args = array())
+    {
+        return $this->_newAction('Create',$args);
+    }
+
+    public function asUpdateAction($args = array())
+    {
+        return $this->_newAction('Update',$args);
+    }
+
+    public function asDeleteAction($args = array())
+    {
+        return $this->_newAction('Delete',$args);
+    }
+
+    /**
+     * Create an action from existing record object
+     *
+     * @param string $type 'create','update','delete'
+     *
+     * TODO: Move to ActionKit
+     */
+    private function _newAction($type, $args = array() )
+    {
+        $class = get_class($this);
+        $actionClass = BaseRecordAction::createCRUDClass($class,$type);
+        return new $actionClass( $args , $this );
+    }
+
+    public function getRecordActionClass($type)
+    {
+        $class = get_class($this);
+        return BaseRecordAction::createCRUDClass($class, $type);
+    }
 
 }
 
