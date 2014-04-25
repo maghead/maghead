2d81b21b685199d73b335bcf1a5db03e9fde5942
c9s <yoanlin93@gmail.com>
2014-04-25 20:36:07 +0800

    Moving class template stuff to src/ClassTemplate for further usage

---
+++ src/ClassTemplate/ClassConst.php
@@ -0,0 +1,20 @@
+<?php
+namespace ClassTemplate;
+
+class ClassConst
+{
+    public $name;
+    public $value;
+
+    public function __construct($name,$value)
+    {
+        $this->name = $name;
+        $this->value = $value;
+    }
+
+    public function __toString()
+    {
+        return 'const ' . $this->name . ' = ' . var_export($this->value,true) . ';';
+    }
+}
+
