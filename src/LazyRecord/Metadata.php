377eb280e9f18c6d46a2a8b7071287a3da1faf45
c9s <yoanlin93@gmail.com>
2014-08-22 12:53:12 +0800

    Add more docblocks to the metadata class.

--- src/LazyRecord/Metadata.php
+++ src/LazyRecord/Metadata.php
@@ -6,6 +6,11 @@ use LazyRecord\TableParser\TableParser;
 use ArrayIterator;
 use PDO;
 
+
+/**
+ * TODO: Extract the key-value storage methods into a KeyValueSchema to 
+ *       generalize the key-value store usecase.
+ */
 class Metadata
     implements ArrayAccess, IteratorAggregate
 {
@@ -27,6 +32,14 @@ class Metadata
      */
     public $driver;
 
+
+    /**
+     * Users can store different metadata in the different data sources.
+     * This constructor requires the first parameter to be the data source Id to 
+     * initialize the metadata object.
+     *
+     * @param string $dsId
+     */
     public function __construct($dsId) 
     {
         $this->dsId = $dsId;
@@ -36,10 +49,17 @@ class Metadata
         $this->init();
     }
 
+
+    /**
+     * This method initialize the metadata table if needed.
+     */
     public function init()
     {
         $parser = TableParser::create( $this->driver, $this->connection );
         $tables = $parser->getTables();
+
+        // if the __meta__table is not found, we should create one to prevent error.
+        // this will be needed for the compatibility of the older version lazyrecord.
         if( ! in_array('__meta__',$tables) ) {
             $schema = new \LazyRecord\Model\MetadataSchema;
             $builder = \LazyRecord\SqlBuilder\SqlBuilder::create($this->driver);
@@ -50,6 +70,11 @@ class Metadata
         }
     }
 
+    /**
+     * Get the current version number from the key-value store.
+     *
+     * @return integer version number
+     */
     public function getVersion()
     {
         if( isset( $this['version'] ) ) {
@@ -58,26 +83,58 @@ class Metadata
         return $this['version'] = 0;
     }
 
-    public function offsetSet($name,$value)
-    {
+
+
+    /**
+     * Check if a key exists in the database.
+     *
+     * @param string $key
+     */
+    public function hasAttribute($key) {
         $stm = $this->connection->prepare('select * from __meta__ where name = :name');
-        $stm->execute(array( ':name' => $name ));
+        $stm->execute(array( ':name' => $key ));
+        $data = $stm->fetch( PDO::FETCH_OBJ );
+        return $data ? true : false;
+    }
+
+    /**
+     * Set an attribute
+     *
+     * @param string $key
+     * @param string $value
+     */
+    public function setAttribute($key, $value) {
+        $stm = $this->connection->prepare('select * from __meta__ where name = :name');
+        $stm->execute(array( ':name' => $key ));
         $obj = $stm->fetch( PDO::FETCH_OBJ );
         if( $obj ) {
             $stm = $this->connection->prepare('update __meta__ set value = :value where name = :name');
-            $stm->execute(array( ':name' => $name, ':value' => $value ));
+            $stm->execute(array( ':name' => $key, ':value' => $value ));
         } else {
             $stm = $this->connection->prepare('insert into __meta__ (name,value) values (:name,:value)');
-            $stm->execute(array( ':name' => $name, ':value' => $value ));
+            $stm->execute(array( ':name' => $key, ':value' => $value ));
         }
     }
 
-    public function offsetExists($name)
+
+
+    /**
+     * Set a value with a key
+     *
+     * @param string $key    the key
+     * @param string $value   the value
+     */
+    public function offsetSet($key,$value)
     {
-        $stm = $this->connection->prepare('select * from __meta__ where name = :name');
-        $stm->execute(array( ':name' => $name ));
-        $data = $stm->fetch( PDO::FETCH_OBJ );
-        return $data ? true : false;
+        return $this->setAttribute($key, $value);
+    }
+
+    /**
+     * Check if a key exists.
+     */
+    public function offsetExists($key)
+    {
+        return $this->hasAttribute($key);
     }
 
     public function offsetGet($name)
@@ -95,9 +152,17 @@ class Metadata
         $stm->execute(array( ':name' => $name ));
     }
 
+    /**
+     * Get iterator for the key-value pair data.
+     *
+     * Please note this method does not cache the meta data, if you call 
+     * this method, this method will do another SQL query to fetch all the attribute
+     *
+     * @return ArrayIterator
+     */
     public function getIterator()
     {
-        $stm = $this->connection->prepare('select * from __meta__');
+        $stm = $this->connection->prepare('SELECT * FROM __meta__');
         $stm->execute();
         $rows = $stm->fetchAll(PDO::FETCH_OBJ);
         $data = array();
