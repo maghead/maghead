cd8aa811d964f08b5c87f1dc671cb5d6ea98b440
c9s <yoanlin93@gmail.com>
2014-09-29 14:15:55 +0800

    Refactor common options to BaseCommand class

---
+++ src/LazyRecord/Command/BaseCommand.php
@@ -0,0 +1,34 @@
+<?php
+namespace LazyRecord\Command;
+use CLIFramework\Command;
+use LazyRecord\ConfigLoader;
+use LazyRecord\Metadata;
+
+class BaseCommand extends Command
+{
+
+    /**
+     * @var ConfigLoader
+     */
+    public $config;
+
+    public function init() {
+        /*
+        $loader = ConfigLoader::getInstance();
+        $loader->loadFromSymbol(true); // force loading
+        $loader->initForBuild();
+        */
+        $this->config = CommandUtils::init_config_loader();
+    }
+
+    public function options($opts)
+    {
+        $self = $this;
+        $opts->add('D|data-source:', 'specify data source id')
+            ->validValues(function() use($self) {
+                return $self->config->getDataSourceIds();
+            })
+            ;
+    }
+
+}
