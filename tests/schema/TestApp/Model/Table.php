33863dd29a3b07246f6076c4567a6d7e9348e076
c9s <yoanlin93@gmail.com>
2014-10-27 00:45:51 +0800

    Add table and table collection

---
+++ tests/schema/TestApp/Model/Table.php
@@ -0,0 +1,16 @@
+<?php
+/**
+This is an auto-generated file,
+Please DO NOT modify this file directly.
+*/
+namespace TestApp\Model;
+
+use TestApp\Model\TableBase;
+
+class Table  extends TableBase {
+
+
+
+
+}
+
