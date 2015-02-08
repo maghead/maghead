eae49fd1ec744cf3bbda379c64c0f1e2eee4f1f3
c9s <yoanlin93@gmail.com>
2015-02-09 01:23:17 +0800

    Add DateTimeTest

--- src/LazyRecord/Types/DateTime.php
+++ src/LazyRecord/Types/DateTime.php
@@ -2,6 +2,9 @@
 namespace LazyRecord\Types;
 use DateTime as PHPDateTime;
 
+/**
+ * Extended DateTime class from PHP built-in DateTime
+ */
 class DateTime extends PHPDateTime
 {
     public function __toString() { return $this->format( PHPDateTime::ATOM ); }
