d2d710c8f6d8ca14527857175325f292881170dd
c9s <yoanlin93@gmail.com>
2015-02-22 15:39:28 +0800

    Add more schema collection tests

--- src/LazyRecord/Schema/SchemaCollection.php
+++ src/LazyRecord/Schema/SchemaCollection.php
@@ -2,24 +2,53 @@
 namespace LazyRecord\Schema;
 use ArrayAccess;
 use IteratorAggregate;
+use Countable;
 use ArrayIterator;
+use LazyRecord\Schema\SchemaDeclare;
+use InvalidArgumentException;
 
-class SchemaCollection implements IteratorAggregate
+class SchemaCollection implements IteratorAggregate, ArrayAccess, Countable
 {
     protected $schemas = array();
 
     public function __construct(array $args) 
     {
         $this->schemas = $args;
-        $this->evaluate();
+    }
+
+    public function filter(callable $cb) {
+        return new self(array_filter($this->schemas, $cb));
+    }
+
+    public function map(callable $cb) {
+        $this->schemas = array_map($this->schemas, $cb);
     }
 
     public function evaluate() 
     {
-        $this->schemas = array_map(function($a) {
-            return is_string($a) ? new $a : $a;
+        $schemas = array_map(function($a) {
+            if (is_string($a)) {
+                return new $a;
+            } elseif (is_object($a)) {
+                return $a;
+            } else {
+                throw new InvalidArgumentException("Invalid schema class argument");
+            }
+            return $a;
         }, $this->schemas);
+        return new self($schemas);
     }
+
+    public function declareable() {
+        return $this->filter(function($schema) {
+            return is_subclass_of($schema, 'LazyRecord\Schema\SchemaDeclare', true);
+        });
+    }
+
+    /*
+        foreach ($classes as $class) {
+        }
+    */
 
     public function getSchemas() 
     {
@@ -31,6 +60,31 @@ class SchemaCollection implements IteratorAggregate
         return array_map(function($a) { return get_class($a); }, $this->schemas);
     }
 
+    public function getBuildableSchemas() 
+    {
+        $list = array();
+        foreach ($this->schemas as $schema) {
+            // skip abstract classes.
+            if (
+              ! is_subclass_of($schema, 'LazyRecord\Schema\SchemaDeclare',true)
+              || is_a($schema, 'LazyRecord\Schema\DynamicSchemaDeclare',true)
+              || is_a($schema, 'LazyRecord\Schema\MixinSchemaDeclare',true)
+              || is_subclass_of($schema, 'LazyRecord\Schema\MixinSchemaDeclare',true)
+            ) { 
+                continue; 
+            }
+
+            // Skip abstract class files...
+            $rf = new ReflectionClass($schema);
+            if ($rf->isAbstract()) {
+                continue;
+            }
+            $list[] = $schema;
+        }
+        return $list;
+    }
+
+
 
     static public function evaluateArray(array $classes) 
     {
@@ -40,11 +94,38 @@ class SchemaCollection implements IteratorAggregate
         return new self($schemas);
     }
 
-
-
     public function getIterator()
     {
         return new ArrayIterator($this->schemas);
+    }
+
+    public function offsetSet($name,$value)
+    {
+        if ($name) {
+            $this->schemas[$name] = $value;
+        } else {
+            $this->schemas[] = $value;
+        }
+    }
+    
+    public function offsetExists($name)
+    {
+        return isset($this->schemas[ $name ]);
+    }
+    
+    public function offsetGet($name)
+    {
+        return $this->schemas[ $name ];
+    }
+    
+    public function offsetUnset($name)
+    {
+        unset($this->schemas[$name]);
+    }
+
+    public function count()
+    {
+        return count($this->schemas);
     }
 }
 
---
+++ tests/LazyRecord/Schema/SchemaCollectionTest.php
@@ -0,0 +1,65 @@
+<?php
+use LazyRecord\Schema\SchemaCollection;
+
+class SchemaCollectionTest extends PHPUnit_Framework_TestCase
+{
+    public function testCountable()
+    {
+        $collection = new SchemaCollection([ 
+            '\TestApp\Model\UserSchema',
+            '\TestApp\Model\IDNumberSchema',
+            '\TestApp\Model\NameSchema',
+            '\AuthorBooks\Model\AddressSchema',
+            '\AuthorBooks\Model\BookSchema',
+            '\AuthorBooks\Model\AuthorSchema',
+            '\AuthorBooks\Model\AuthorBookSchema',
+            '\AuthorBooks\Model\PublisherSchema',
+        ]);
+        $this->assertEquals(8, $collection->count());
+    }
+
+    public function testEvaluate() 
+    {
+        $c = new SchemaCollection([ 
+            '\TestApp\Model\UserSchema',
+            '\TestApp\Model\IDNumberSchema',
+            '\TestApp\Model\NameSchema',
+            '\AuthorBooks\Model\AddressSchema',
+            '\AuthorBooks\Model\BookSchema',
+            '\AuthorBooks\Model\AuthorSchema',
+            '\AuthorBooks\Model\AuthorBookSchema',
+            '\AuthorBooks\Model\PublisherSchema',
+        ]);
+
+        foreach ($c->getSchemas() as $s) {
+            $this->assertInternalType('string', $s);
+        }
+
+        $ec = $c->evaluate();
+        foreach ($ec->getSchemas() as $s) {
+            $this->assertInstanceOf('LazyRecord\Schema\SchemaDeclare', $s);
+        }
+    }
+
+    public function testFilter() 
+    {
+        $c = new SchemaCollection([ 
+            '\TestApp\Model\UserSchema',
+            '\TestApp\Model\IDNumberSchema',
+            '\TestApp\Model\NameSchema',
+            '\AuthorBooks\Model\AddressSchema',
+            '\AuthorBooks\Model\BookSchema',
+            '\AuthorBooks\Model\AuthorSchema',
+            '\AuthorBooks\Model\AuthorBookSchema',
+            '\AuthorBooks\Model\PublisherSchema',
+        ]);
+
+        $this->assertEquals(8, count($c));
+
+        $rc = $c->evaluate()->filter(function($schema) {
+            return $schema instanceof \AuthorBooks\Model\BookSchema;
+        });
+        $this->assertEquals(1, count($rc));
+    }
+}
+
