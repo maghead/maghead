508935ad78aed0a16743b9647c7300f02c4fc40e
c9s <yoanlin93@gmail.com>
2015-04-16 11:18:33 +0800

    Fix tests

--- tests/LazyRecord/Schema/SchemaDeclare/ColumnTest.php
+++ tests/LazyRecord/Schema/SchemaDeclare/ColumnTest.php
@@ -1,20 +1,17 @@
 <?php
-
 class ColumnTest extends PHPUnit_Framework_TestCase
 {
     function test()
     {
         $column = new LazyRecord\Schema\ColumnDeclare('foo');
-        ok($column);
-
         $column->primary()
             ->integer()
             ->autoIncrement()
             ->notNull();
 
-        is('foo',$column->name);
-        ok($column->primary);
-        is('integer',$column->type);
+        $this->assertEquals('foo',$column->name);
+        $this->assertTrue($column->primary);
+        $this->assertEquals('int',$column->type);
         $this->assertFalse($column->null);
     }
 }
