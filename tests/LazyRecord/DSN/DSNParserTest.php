7cf3e3102ceb5aacbefaa871198de8883a8e50d1
c9s <yoanlin93@gmail.com>
2015-07-27 12:00:40 +0800

    test DSN with space separator

--- tests/LazyRecord/DSN/DSNParserTest.php
+++ tests/LazyRecord/DSN/DSNParserTest.php
@@ -13,6 +13,7 @@ class DSNParserTest extends PHPUnit_Framework_TestCase
             ['mysql:host=localhost;port=3307;dbname=testdb'],
             ['mysql:unix_socket=/tmp/mysql.sock;dbname=testdb'],
             ['pgsql:host=localhost;port=5432;dbname=testdb;user=bruce;password=mypass'],
+            ['pgsql:user=exampleuser dbname=exampledb password=examplepass'],
         ];
     }
 
