051f332fad60feba0f72a9b44fdd04d1391ba303
c9s <yoanlin93@gmail.com>
2015-07-28 16:28:46 +0800

    Implement set-default command for setting default data-source

--- src/LazyRecord/Command/DataSourceCommand/SetDefaultCommand.php
+++ src/LazyRecord/Command/DataSourceCommand/SetDefaultCommand.php
@@ -7,6 +7,9 @@ use LazyRecord\DSN\DSNParser;
 use SQLBuilder\Driver\PDODriverFactory;
 use SQLBuilder\ArgumentArray;
 use SQLBuilder\Universal\Query\CreateDatabaseQuery;
+use Symfony\Component\Yaml\Yaml;
+use Symfony\Component\Yaml\Dumper;
+use Exception;
 use PDO;
 
 class SetDefaultCommand extends BaseCommand
@@ -26,41 +29,36 @@ class SetDefaultCommand extends BaseCommand
         // force loading data source
         $configLoader = $this->getConfigLoader(true);
 
-        $idList = $configLoader->getDataSourceIdList();
+        $dataSources = $configLoader->getDataSources();
 
-
-
-        $dsId = $this->getCurrentDataSourceId();
-        $ds = $configLoader->getDataSource($dsId);
-
-        $dsnParser = new DSNParser;
-        $dsn = $dsnParser->parse($ds['dsn']);
-
-        $dbName = $dsn->getAttribute('dbname');
-
-        $dsn->removeAttribute('dbname');
-
-        $this->logger->debug("Connection DSN: " . $dsn);
-
-        $pdo = new PDO($dsn, @$ds['user'], @$ds['pass'], @$ds['connection_options']);
-
-        $q = new CreateDatabaseQuery($dbName);
-        if (isset($ds['charset'])) {
-            $q->characterSet($ds['charset']);
-        } else {
-            $q->characterSet('utf8');
-        }
-
-        $queryDriver = PDODriverFactory::create($pdo);
-        $sql = $q->toSql($queryDriver, new ArgumentArray);
-        $this->logger->info($sql);
-
-        if ($pdo->query($sql) === false) {
-            list($statusCode, $errorCode, $message) = $pdo->errorInfo();
-            $this->logger->error("$statusCode:$errorCode $message");
+        if (!in_array($defaultDataSource, array_keys($dataSources))) {
+            $this->logger->error("Undefined data source ID: $defaultDataSource");
             return false;
         }
-        $this->logger->info('Database created successfully.');
+
+        $config = $configLoader->getConfigStash();
+        $config['data_source']['default'] = $defaultDataSource;
+
+        $this->logger->debug("Checking symbol link file: " . $configLoader->symbolFilename);
+        if (!file_exists($configLoader->symbolFilename)) {
+            $this->logger->error($configLoader->symbolFilename . " is missing. please use lazy build-conf {filename} to update your config link.");
+            return false;
+        }
+
+        $targetFile = readlink($configLoader->symbolFilename);
+        if ($targetFile === false || !file_exists($targetFile)) {
+            $this->logger->error('Missing target config file. incorrect symbol link.');
+            return false;
+        }
+
+        $this->logger->debug("Writing config back to $targetFile");
+
+        $yaml = Yaml::dump($config, $inlineLevel = 4, $indentSpaces = 2, $exceptionOnInvalidType = true);
+        if (false === file_put_contents($targetFile, "---\n" . $yaml)) {
+            $this->logger->error("YAML config update failed: $targetFile");
+            return false;
+        }
+        $this->logger->info("Config file is updated successfully.");
     }
 
 }
