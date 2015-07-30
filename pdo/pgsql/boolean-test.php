fc955e466871bdf6447e9bd0b5420cec8a71421f
c9s <yoanlin93@gmail.com>
2015-07-30 15:09:13 +0800

    Enable debug mode

--- pdo/pgsql/boolean-test.php
+++ pdo/pgsql/boolean-test.php
@@ -1,6 +1,6 @@
 <?php
 require 'vendor/autoload.php';
-use CLIFramework\Component\Table\Table;
+require __DIR__ . '/../utils.php';
 
 $dbh = new PDO('pgsql:host=localhost;dbname=testing', 'postgres');
 // $dbh->query('create table authors (confirmed BOOL default false);');
@@ -26,39 +26,7 @@ if (false === $stm->execute([':confirmed' => false, ':name' => 'false'])) {
 }
 
 
-function dumpQuery($dbh, $query)
-{
-    $stm = $dbh->query($query);
-    if ($stm == false) {
-        var_dump($dbh->errorInfo());
-        return;
-    }
-
-    echo "Result for: $query\n";
-    $rows = $stm->fetchAll(PDO::FETCH_ASSOC);
-    $table = new Table;
-    $table->setHeaders(array_keys($rows[0]));
-    foreach($rows as $row) {
-        $values = array_map(function($val) { return var_export($val, true); }, array_values($row));
-        $table->addRow($values);
-    }
-
-    echo $table->render();
-}
 dumpQuery($dbh,'select * from authors WHERE confirmed is false');
+dumpQuery($dbh,'select * from authors WHERE confirmed = false');
 dumpQuery($dbh,'select * from authors WHERE confirmed is true');
-// var_dump($dbh->query('select * from authors WHERE confiremd = 0')->fetchAll());
-/*
-$dbh = new PDO('mysql:host=localhost;dbname=testing', 'root', 'qwer1234');
-$stm = $dbh->prepare('insert into authors (name) values (:name)');
-$stm->execute([ ':name' => 'John' ]);
-
-var_dump($dbh->query('select * from authors')->fetchAll());
-
-$stm = $dbh->prepare('update authors set name = :name');
-$stm->execute([ ':name' => null ]);
-
-var_dump($dbh->query('select * from authors')->fetchAll());
-
-$dbh->query('delete from authors');
- */
+dumpQuery($dbh,'select * from authors WHERE confirmed = true');
