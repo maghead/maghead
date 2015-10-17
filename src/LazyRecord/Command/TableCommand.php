45acf136ac8d5c8fbe3ff252a8c19469168d390f
c9s <yoanlin93@gmail.com>
2015-10-17 11:37:20 +0800

    Table Status: Add table command to show table status
    - table status with pretty size format
    - table status layout improvement for Mysql

---
+++ src/LazyRecord/Command/TableCommand.php
@@ -0,0 +1,72 @@
+<?php
+namespace LazyRecord\Command;
+use CLIFramework\Command;
+use LazyRecord\Command\BaseCommand;
+use LazyRecord\TableStatus\MySQLTableStatus;
+use LazyRecord\ConnectionManager;
+use SQLBuilder\Driver\PDOMySQLDriver;
+use CLIFramework\Component\Table\Table;
+
+class TableCommand extends BaseCommand
+{
+    public function brief()
+    {
+        return 'show table status.';
+    }
+
+    public function aliases()
+    {
+        return ['tables', 't'];
+    }
+
+    public function options($opts)
+    {
+        parent::options($opts);
+        $opts->add('v|verbose', 'Display verbose information');
+    }
+
+    public function execute()
+    {
+        $tables = func_get_args();
+        $configLoader = $this->getConfigLoader(true);
+
+        $dataSource = $this->getCurrentDataSourceId();
+        $conn = $this->getCurrentConnection();
+        $driver = $this->getCurrentQueryDriver();
+
+        if ($driver instanceof PDOMySQLDriver) {
+            $status = new MySQLTableStatus($conn, $driver);
+
+            $this->logger->info("Table Status:");
+            $rows = $status->queryDetails($tables);
+            $this->displayRows($rows);
+
+            $this->logger->newline();
+            $this->logger->info("Table Status Summary:");
+            $rows = $status->querySummary($tables);
+            $this->displayRows($rows);
+        } else {
+            $this->logger->error("Driver not supported.");
+        }
+
+    }
+
+    protected function displayRows(array $rows)
+    {
+        if (count($rows)) {
+            $table = new Table();
+            $headers = array_keys($rows[0]);
+            $table->setHeaders($headers);
+            foreach ($rows as $row) {
+                $table->addRow(array_values($row));
+            }
+            echo $table->render();
+        }
+    }
+
+}
+
+
+
+
+
---
+++ src/LazyRecord/TableStatus/MySQLTableStatus.php
@@ -0,0 +1,126 @@
+<?php
+namespace LazyRecord\TableStatus;
+use PDO;
+use SQLBuilder\Driver\BaseDriver;
+use SQLBuilder\Driver\PDOMySQLDriver;
+use SQLBuilder\Universal\Query\SelectQuery;
+use SQLBuilder\ArgumentArray;
+use CLIFramework\Component\Table\Table;
+
+class MySQLTableStatus
+{
+
+    protected $connection;
+
+    protected $driver;
+
+    public function __construct(PDO $connection, PDOMySQLDriver $driver)
+    {
+        $this->connection = $connection;
+        $this->driver = $driver;
+    }
+
+
+
+    protected function createStatusSummaryQuery()
+    {
+        $query = new SelectQuery;
+        $query->select([
+            'CONCAT(table_schema, \'.\', table_name) AS name',
+            'CONCAT(ROUND(SUM(table_rows) / 1000000, 2), \'M\') AS rows',
+            'CASE WHEN SUM(data_length) > 1024 * 1024 * 1024 THEN CONCAT(ROUND(SUM(data_length) / (1024 * 1024 * 1024), 2), \'G\')
+                  WHEN SUM(data_length) > 1024 * 1024        THEN CONCAT(ROUND(SUM(data_length) / (1024 * 1024), 2), \'M\')
+                                                        ELSE CONCAT(ROUND(SUM(data_length) / 1024, 2), \'K\')
+                                                        END AS data_size',
+            'CASE WHEN SUM(index_length) > 1024 * 1024 * 1024 THEN CONCAT(ROUND(SUM(index_length) / (1024 * 1024 * 1024), 2), \'G\')
+                  WHEN SUM(index_length) > 1024 * 1024        THEN CONCAT(ROUND(SUM(index_length) / (1024 * 1024), 2), \'M\')
+                                                        ELSE CONCAT(ROUND(SUM(index_length) / (1024), 2), \'K\')
+                                                        END AS index_size',
+            'CASE WHEN SUM(data_length+index_length) > 1024 * 1024 * 1024 THEN CONCAT(ROUND(SUM(data_length+index_length) / (1024 * 1024 * 1024), 2), \'G\')
+                  WHEN SUM(data_length+index_length) > 1024 * 1024        THEN CONCAT(ROUND(SUM(data_length+index_length) / (1024 * 1024), 2), \'M\')
+                                                        ELSE CONCAT(ROUND(SUM(data_length+index_length) / (1024), 2), \'K\')
+                                                        END AS total_size',
+        ]);
+        $query->from('information_schema.TABLES');
+        return $query;
+    }
+
+
+
+    protected function createStatusDetailQuery()
+    {
+        $query = new SelectQuery;
+        $query->select([
+            'CONCAT(table_schema, \'.\', table_name) AS name',
+            'CONCAT(ROUND(table_rows / 1000000, 2), \'M\') AS rows',
+
+            'CASE WHEN data_length > 1024 * 1024 * 1024 THEN CONCAT(ROUND(data_length / (1024 * 1024 * 1024), 2), \'G\')
+                  WHEN data_length > 1024 * 1024        THEN CONCAT(ROUND(data_length / (1024 * 1024), 2), \'M\')
+                                                        ELSE CONCAT(ROUND(data_length / (1024), 2), \'K\')
+                                                        END AS data_size',
+
+            'CASE WHEN index_length > 1024 * 1024 * 1024 THEN CONCAT(ROUND(index_length / (1024 * 1024 * 1024), 2), \'G\')
+                  WHEN index_length > 1024 * 1024        THEN CONCAT(ROUND(index_length / (1024 * 1024), 2), \'M\')
+                                                        ELSE CONCAT(ROUND(index_length / (1024), 2), \'K\')
+                                                        END AS index_size',
+
+            'CASE WHEN (data_length+index_length) > 1024 * 1024 * 1024 THEN CONCAT(ROUND((data_length+index_length) / (1024 * 1024 * 1024), 2), \'G\')
+                  WHEN (data_length+index_length) > 1024 * 1024        THEN CONCAT(ROUND((data_length+index_length) / (1024 * 1024), 2), \'M\')
+                                                        ELSE CONCAT(ROUND((data_length+index_length) / (1024), 2), \'K\')
+                                                        END AS total_size',
+
+            'ROUND(index_length / data_length, 2) AS index_frac',
+        ]);
+        $query->from('information_schema.TABLES');
+        $query->orderBy('data_length + index_length', 'DESC');
+        return $query;
+    }
+
+
+    public function querySummary($tables)
+    {
+        $dbName = $this->connection->query('SELECT database();')->fetchColumn();
+        $query = $this->createStatusSummaryQuery();
+        $query->where()->equal('table_schema', $dbName);
+        if (count($tables)) {
+            $query->where()
+                ->in('table_name', $tables);
+        }
+        $args = new ArgumentArray;
+        $sql = $query->toSql($this->driver, $args);
+        $stm = $this->connection->prepare($sql);
+        $stm->execute($args->toArray());
+        return $rows = $stm->fetchAll(PDO::FETCH_ASSOC);
+
+    }
+
+    public function queryDetails(array $tables)
+    {
+        $dbName = $this->connection->query('SELECT database();')->fetchColumn();
+        /*
+        SELECT 
+            CONCAT(table_schema, '.', table_name),
+            CONCAT(ROUND(table_rows / 1000000, 2), 'M') AS rows,
+            CONCAT(ROUND(data_length / ( 1024 * 1024 * 1024 ), 2), 'G') AS data,
+            CONCAT(ROUND(index_length / ( 1024 * 1024 * 1024 ), 2), 'G') AS idx,
+            CONCAT(ROUND(( data_length + index_length ) / ( 1024 * 1024 * 1024 ), 2), 'G') AS total_size,
+            ROUND(index_length / data_length, 2) AS idxfrac
+            FROM information_schema.TABLES 
+            WHERE table_schema = 'bossnet' ORDER  BY data_length + index_length DESC LIMIT  10;
+        */
+        $query = $this->createStatusDetailQuery();
+        $query->where()->equal('table_schema', $dbName);
+        if (count($tables)) {
+            $query->where()
+                ->in('table_name', $tables);
+        }
+        $args = new ArgumentArray;
+        $sql = $query->toSql($this->driver, $args);
+        $stm = $this->connection->prepare($sql);
+        $stm->execute($args->toArray());
+        return $stm->fetchAll(PDO::FETCH_ASSOC);
+    }
+}
+
+
+
