741540f58b189f542aa88ae51b81d3bf38c3cbf9
c9s <yoanlin93@gmail.com>
2015-10-18 00:41:44 +0800

    Add more missing schemas to bootstrap script for updating schema files

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -20,17 +20,23 @@ $logger->info("Building schema class files...");
 
 // build schema class files
 $schemas = array(
-    new \TestApp\Model\UserSchema,
-    new \TestApp\Model\IDNumberSchema,
-    new \TestApp\Model\NameSchema,
     new \AuthorBooks\Model\AddressSchema,
-    new \AuthorBooks\Model\BookSchema,
-    new \AuthorBooks\Model\AuthorSchema,
     new \AuthorBooks\Model\AuthorBookSchema,
+    new \AuthorBooks\Model\AuthorSchema,
+    new \AuthorBooks\Model\BookSchema,
     new \AuthorBooks\Model\PublisherSchema,
+    new \AuthorBooks\Model\TagSchema,
     new \MetricApp\Model\MetricValueSchema,
     new \PageApp\Model\PageSchema,
     new \StoreApp\Model\StoreSchema,
+    new \TestApp\Model\EdmSchema,
+    new \TestApp\Model\IDNumberSchema,
+    new \TestApp\Model\NameSchema,
+    new \TestApp\Model\PostSchema,
+    new \TestApp\Model\TableSchema,
+    new \TestApp\Model\UserSchema,
+    new \TestApp\Model\WineCategorySchema,
+    new \TestApp\Model\WineSchema,
 );
 $g = new \LazyRecord\Schema\SchemaGenerator($config, $logger);
 $g->setForceUpdate(true);
