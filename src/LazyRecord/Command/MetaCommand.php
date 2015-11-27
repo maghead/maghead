0fd9b00b3e3ca4edb24608528c47efb20b93cd81
c9s <yoanlin93@gmail.com>
2015-11-27 22:40:00 +0800

    Fix Meta command

--- src/LazyRecord/Command/MetaCommand.php
+++ src/LazyRecord/Command/MetaCommand.php
@@ -17,27 +17,27 @@ class MetaCommand extends BaseCommand
     public function execute() 
     {
         $dsId = $this->getCurrentDataSourceId();
+        $queryDriver = $this->getCurrentQueryDriver();
+        $conn = $this->getCurrentConnection();
         
 
         $args = func_get_args();
         if(empty($args)) {
-            $meta = new Metadata($dsId);
+            $meta = new Metadata($queryDriver, $conn);
             printf("%26s | %-20s\n",'Key','Value');
             printf("%s\n", str_repeat('=',50));
             foreach( $meta as $key => $value ) {
                 printf("%26s   %-20s\n", $key, $value);
             }
-        }
-        elseif( count($args) == 1 ) {
+        } else if (count($args) == 1) {
             $key = $args[0];
-            $meta = new Metadata($dsId);
+            $meta = new Metadata($queryDriver, $conn);
             $value = $meta[$key];
             $this->logger->info("$key = $value");
-        }
-        elseif( count($args) == 2 ) {
+        } else if (count($args) == 2) {
             list($key,$value) = $args;
             $this->logger->info("Setting meta $key to $value.");
-            $meta = new Metadata($dsId);
+            $meta = new Metadata($queryDriver, $conn);
             $meta[$key] = $value;
         }
     }
