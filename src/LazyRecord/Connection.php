1d4b73d2952ebdc49565485ffc259cc0a60742b9
c9s <yoanlin93@gmail.com>
2015-12-01 15:07:09 +0800

    Add DSN parser to Connection object

--- src/LazyRecord/Connection.php
+++ src/LazyRecord/Connection.php
@@ -3,18 +3,50 @@ namespace LazyRecord;
 use SQLBuilder\Driver\MySQLDriver;
 use SQLBuilder\Driver\PDODriverFactory;
 use PDO;
+use LazyRecord\DSN\DSNParser;
+use LazyRecord\DSN\DSN;
 
 class Connection extends PDO
 {
-    public function prepareAndExecute($sql, $args = array())
+    protected $config;
+
+    private $dsn;
+
+    static public function create(array $config)
+    {
+        $connection = new self($config['dsn'], $config['user'], $config['pass'], $config['connection_options']);
+        $connection->config = $config;
+        return $connection;
+    }
+
+    public function setConfig(array $config)
+    {
+        $this->config = $config;
+    }
+
+    public function prepareAndExecute($sql, array $args = array())
     {
         $stm = $this->prepare($sql);
         $stm->execute($args); // $success 
         return $stm;
     }
 
-    public function createQueryDriver() {
+    public function createQueryDriver()
+    {
         return PDODriverFactory::create($this);
+    }
+
+    public function getConfig()
+    {
+        return $this->config;
+    }
+
+    public function getDSN()
+    {
+        if ($this->dsn) {
+            return $this->dsn;
+        }
+        return $this->dsn = DSNParser::parse($this->config['dsn']);
     }
 }
 
