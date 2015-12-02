fc531891074b251c5b5d7741a67dde22f3a2bbea
c9s <yoanlin93@gmail.com>
2015-12-02 14:27:44 +0800

    Add schema status command

---
+++ src/LazyRecord/Command/SchemaCommand/StatusCommand.php
@@ -0,0 +1,61 @@
+<?php
+namespace LazyRecord\Command\SchemaCommand;
+use LazyRecord\Command\BaseCommand;
+use LazyRecord\Schema\SchemaGenerator;
+use LazyRecord\Schema\SchemaUtils;
+
+/**
+ * $ lazy build-schema path/to/Schema path/to/SchemaDir
+ *
+ */
+class StatusCommand extends BaseCommand
+{
+
+    public function usage()
+    {
+        return 'schema status';
+    }
+
+    public function brief()
+    {
+        return 'show schema status.';
+    }
+
+    public function arguments($args)
+    {
+        /*
+        $args->add('file')
+            ->isa('file')
+            ;
+         */
+    }
+
+    public function options($opts) 
+    {
+        $opts->add('f|force','force generate all schema files.');
+        parent::options($opts);
+    }
+
+    public function execute()
+    {
+        $logger = $this->getLogger();
+
+
+        $config = $this->getConfigLoader();
+
+        $this->logger->debug('Finding schemas...');
+        $classes = $this->findSchemasByArguments(func_get_args());
+
+        $this->logger->debug("Initializing schema generator...");
+
+        $generator = new SchemaGenerator($config, $this->logger);
+
+        if ($this->options->force) {
+            $generator->setForceUpdate(true);
+        }
+        $classMap = $generator->generate($classes, $this->options->force);
+        $logger->info('Done');
+    }
+
+}
+
