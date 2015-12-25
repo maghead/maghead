1093995a9f4be2d199c9c4f16c3cd09bc5b029f0
c9s <yoanlin93@gmail.com>
2015-12-25 19:38:17 +0800

    Support intersectKeys to csv exporter

--- src/LazyRecord/Exporter/CSVExporter.php
+++ src/LazyRecord/Exporter/CSVExporter.php
@@ -44,13 +44,18 @@ class CSVExporter
         $this->escapeChar = $char;
     }
 
-    public function exportQuery(PDOStatement $stm)
+    public function exportQuery(PDOStatement $stm, array $intersectKeys = null)
     {
         $all = $stm->fetchAll(PDO::FETCH_ASSOC);
         if (empty($all)) {
             return false;
         }
         $columns = array_keys($all[0]);
+
+        if ($intersectKeys) {
+            $columns = array_intersect_key($columns, array_flip($intersectKeys));
+        }
+
         $php54 = version_compare(phpversion(), '5.5.0') < 0;
         if ($php54) {
             fputcsv($this->fd, $columns, $this->delimiter, $this->enclosure);
@@ -77,10 +82,14 @@ class CSVExporter
      *
      * int fputcsv ( resource $handle , array $fields [, string $delimiter = "," [, string $enclosure = '"' [, string $escape_char = "\" ]]] )
      */
-    public function exportCollection(BaseCollection $collection)
+    public function exportCollection(BaseCollection $collection, array $intersectKeys = null)
     {
         $schema = $collection->getSchema();
         $keys = $schema->getColumnNames();
+
+        if ($intersectKeys) {
+            $keys = array_intersect_key($keys, array_flip($intersectKeys));
+        }
 
         $php54 = version_compare(phpversion(), '5.5.0') < 0;
         if ($php54) {
