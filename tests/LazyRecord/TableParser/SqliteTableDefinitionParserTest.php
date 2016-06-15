a31d033720c053e5a73da3a5e6558aab011812cf
c9s <yoanlin93@gmail.com>
2016-06-15 22:31:38 +0800

    parse unsigned keyword in sqlite table def parser

--- tests/LazyRecord/TableParser/SqliteTableDefinitionParserTest.php
+++ tests/LazyRecord/TableParser/SqliteTableDefinitionParserTest.php
@@ -3,6 +3,21 @@ use LazyRecord\TableParser\SqliteTableDefinitionParser;
 
 class SqliteTableDefinitionParserTest extends PHPUnit_Framework_TestCase
 {
+
+
+    public function testUnsignedInt()
+    {
+        $parser = new SqliteTableDefinitionParser('CREATE TABLE foo (`a` INT UNSIGNED DEFAULT 123)');
+        $def = $parser->parse();
+        $this->assertNotNull($def);
+        $this->assertEquals('foo',$def->tableName);
+        $this->assertCount(1,$def->columns->columns);
+        $this->assertEquals('a',$def->columns->columns[0]->name);
+        $this->assertEquals('INT',$def->columns->columns[0]->type);
+        $this->assertEquals("123",$def->columns->columns[0]->default);
+    }
+
+
     /**
      * @see https://github.com/c9s/LazyRecord/issues/94
      */
@@ -17,6 +32,5 @@ class SqliteTableDefinitionParserTest extends PHPUnit_Framework_TestCase
         $this->assertEquals('col4',$def->columns->columns[0]->name);
         $this->assertEquals('TEXT',$def->columns->columns[0]->type);
         $this->assertEquals("123\\'",$def->columns->columns[0]->default);
-        // echo json_encode( $def , JSON_PRETTY_PRINT);
     }
 }
