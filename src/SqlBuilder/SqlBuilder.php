327c8f3110a3f8942d564fd0780760e56a1d5be0
c9s <yoanlin93@gmail.com>
2016-06-17 12:51:53 +0800

    Fix class names

--- src/SqlBuilder/SqlBuilder.php
+++ src/SqlBuilder/SqlBuilder.php
@@ -13,19 +13,12 @@ class SqlBuilder
     public static function create(BaseDriver $driver, array $options = array())
     {
         if ($driver instanceof MySQLDriver) {
-            return new MySQLBuilder($driver, $options);
-        } elseif ($driver instanceof PgSQLDriver) {
-            return new PgSQLBuilder($driver, $options);
-        } elseif ($driver instanceof SQLiteDriver) {
-            return new SQLiteBuilder($driver, $options);
+            return new MysqlBuilder($driver, $options);
+        } else if ($driver instanceof PgSQLDriver) {
+            return new PgsqlBuilder($driver, $options);
+        } else if ($driver instanceof SQLiteDriver) {
+            return new SqliteBuilder($driver, $options);
         }
-
-        $className = get_class($driver);
-        preg_match('/PDO(\w+)Driver$/', $className, $regs);
-        if (!$regs[1]) {
-            throw new Exception("Can't create sqlbuilder driver class from: ".get_class($driver));
-        }
-        $class = '\\LazyRecord\\SqlBuilder\\'.ucfirst(strtolower($regs[1])).'Builder';
-        return new $class($driver, $options);
+        throw new Exception("Unsupported driver");
     }
 }
