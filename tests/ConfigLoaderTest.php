cf0f18cdff66254d5015fe28b71027b33c50e439
c9s <yoanlin93@gmail.com>
2016-06-19 21:56:09 +0800

    Improve ConfigLoaderTest

--- tests/ConfigLoaderTest.php
+++ tests/ConfigLoaderTest.php
@@ -3,22 +3,19 @@ use LazyRecord\ConfigLoader;
 
 class ConfigLoaderTest extends PHPUnit_Framework_TestCase
 {
+    public function testLoadFromSymbol()
+    {
+        $config = new ConfigLoader;
+        $config->loadFromSymbol(true);
+        $config->setDefaultDataSourceId('mysql');
+        $this->assertEquals('mysql', $config->getDefaultDataSourceId());
+    }
+
+
     public function testSetConfigStash()
     {
         $config = new ConfigLoader;
         $config->setLoaded(true);
         $config->setConfigStash(array( 'schema' => array( 'auto_id' => true ) ));
-        ok($config);
     }
-
-    public function testLoadFromSymbol()
-    {
-        $config = new ConfigLoader;
-        $config->loadFromSymbol();
-        $this->assertTrue($config->loaded);
-    }
-
-
-
 }
-
