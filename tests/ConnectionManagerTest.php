d93022d5cd5f89d8b98e9d657bab525c48286d27
c9s <yoanlin93@gmail.com>
2016-06-19 21:42:03 +0800

    Add query driver assertion

--- tests/ConnectionManagerTest.php
+++ tests/ConnectionManagerTest.php
@@ -1,27 +1,43 @@
 <?php
 use LazyRecord\Connection;
+use LazyRecord\ConnectionManager;
+use LazyRecord\ConfigLoader;
+use SQLBuilder\Driver\PDOMySQLDriver;
 
 class ConnectionManagerTest extends PHPUnit_Framework_TestCase
 {
+
+
+    public function testDefaultDataSource()
+    {
+        if (getenv('DB') != 'mysql') {
+            return $this->markTestSkipped('require mysql to test');
+        }
+        $configLoader = ConfigLoader::getInstance();
+        $configLoader->loadFromSymbol(true);
+        $configLoader->setDefaultDataSourceId('mysql');
+        $this->assertEquals('mysql',$configLoader->getDefaultDataSourceId());
+
+        $connManager = ConnectionManager::getInstance();
+        $connManager->init($configLoader);
+        $this->assertEquals('mysql',$configLoader->getDefaultDataSourceId());
+
+        $conn = $connManager->getConnection('default');
+        $queryDriver = $conn->createQueryDriver();
+        $this->assertInstanceOf('SQLBuilder\Driver\PDOMySQLDriver', $queryDriver);
+        $connManager->free();
+    }
+
+
+    /*
     public function testConnectionManager()
     {
         // $pdo = new PDO( 'sqlite3::memory:', null, null, array(PDO::ATTR_PERSISTENT => true) );
-        $conn = new Connection( 'sqlite::memory:' );
-        ok( $conn );
-
+        $conn = new Connection('sqlite::memory:');
         $manager = LazyRecord\ConnectionManager::getInstance();
-        ok( $manager );
-
         $manager->free();
-
-        $manager->add($conn, 'default');
-
-        /*
-        $conn = $manager->getDefaultConnection();
-        ok($conn);
-         */
-
-        $manager->addDataSource( 'master', array( 
+        $manager->add($conn, 'master');
+        $manager->addDataSource( 'master', array(
             'dsn' => 'sqlite::memory:',
             'user' => null,
             'pass' => null,
@@ -29,14 +45,13 @@ class ConnectionManagerTest extends PHPUnit_Framework_TestCase
         ));
 
         $master = $manager->getConnection('master');
-        ok( $master );
-
+        $this->assertInstanceOf('LazyRecord\Connection',$master);
 
         // array access test.
-        ok( $manager['master'] );
-
+        ok($manager['master']);
         $manager->free();
     }
+    */
 }
 
 
