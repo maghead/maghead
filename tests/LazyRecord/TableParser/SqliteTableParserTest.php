da41401e8991984f878afb381ead54a43b0c9a21
c9s <yoanlin93@gmail.com>
2016-06-19 17:53:32 +0800

    Fix tests

--- tests/LazyRecord/TableParser/SqliteTableParserTest.php
+++ tests/LazyRecord/TableParser/SqliteTableParserTest.php
@@ -8,8 +8,6 @@ class SqliteTableParserTest extends PHPUnit_Framework_TestCase
     {
         $pdo = new PDO('sqlite::memory:');
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        ok($pdo);
-
         $pdo->query('CREATE TABLE foo ( id integer primary key autoincrement, name varchar(12), phone varchar(32) unique , address text not null );');
         $pdo->query('CREATE TABLE bar ( id integer primary key autoincrement, confirmed boolean default false, content blob );');
 
@@ -20,8 +18,6 @@ class SqliteTableParserTest extends PHPUnit_Framework_TestCase
         $this->assertCount(2, $tables);
 
         $sql = $parser->getTableSql('foo');
-        ok($sql);
-
         $columns = $parser->parseTableSql('foo');
         $this->assertNotEmpty($columns);
 
@@ -34,7 +30,7 @@ class SqliteTableParserTest extends PHPUnit_Framework_TestCase
         $id = $schema->getColumn('id');
         $this->assertNotNull($id);
         $this->assertTrue($id->autoIncrement);
-        $this->assertEquals('INTEGER',$id->type);
+        $this->assertEquals('INT',$id->type);
         $this->assertEquals('int',$id->isa);
         $this->assertTrue($id->primary);
     }
