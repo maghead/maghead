fc3c06e5d7fce099159684e6eb41d5734357b35a
c9s <yoanlin93@gmail.com>
2017-01-28 18:59:14 +0800

    Remove breakDSN method

--- src/Utils.php
+++ src/Utils.php
@@ -7,35 +7,6 @@ use Maghead\Schema\SchemaLoader;
 
 class Utils
 {
-    public static function breakDSN($dsn)
-    {
-        // break DSN string down into parameters
-        $params = array();
-        if (strpos($dsn, ':') === false) {
-            $params['driver'] = $dsn;
-
-            return $params;
-        }
-
-        list($driver, $paramString) = explode(':', $dsn, 2);
-        $params['driver'] = $driver;
-
-        if ($paramString === ':memory:') {
-            $params[':memory:'] = 1;
-
-            return $params;
-        }
-
-        $paramPairs = explode(';', $paramString);
-        foreach ($paramPairs as $pair) {
-            if (preg_match('#(\S+)=(\S+)#', $pair, $regs)) {
-                $params[$regs[1]] = $regs[2];
-            }
-        }
-
-        return $params;
-    }
-
     public static function evaluate($data, $params = array())
     {
         if ($data && is_callable($data)) {
--- tests/UtilsTest.php
+++ tests/UtilsTest.php
@@ -3,21 +3,6 @@ use Maghead\Utils;
 
 class UtilsTest extends PHPUnit_Framework_TestCase
 {
-    public function testBreakDSN()
-    {
-        $params = Utils::breakDSN('pgsql:host=localhost;dbname=lazy_test');
-        $this->assertEquals( 'pgsql' , $params['driver'] );
-        $this->assertEquals( 'localhost' , $params['host'] );
-        $this->assertEquals( 'lazy_test' , $params['dbname'] );
-    }
-
-    public function testSqliteMemDSN()
-    {
-        $params = Utils::breakDSN('sqlite::memory:');
-        $this->assertEquals( 'sqlite' , $params['driver'] );
-        ok( $params[':memory:'] );
-    }
-
     public function testEvaluateFunction()
     {
         $this->assertEquals( 1, Utils::evaluate(1) );
