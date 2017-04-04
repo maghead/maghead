2d315b1b7564b27ed4bab3bb27f4a47660824bf3
c9s <yoanlin93@gmail.com>
2017-04-05 00:22:25 +0800

    refactor store test case methods

--- tests/apps/StoreApp/StoreTestCase.php
+++ tests/apps/StoreApp/StoreTestCase.php
@@ -14,52 +14,76 @@ abstract class StoreTestCase extends ModelTestCase
 {
     protected $defaultDataSource = 'node_master';
 
-    protected $requiredDataSources = ['node_master', 'node1', 'node2'];
+    protected $requiredDataSources = ['node_master', 'node1', 'node2', 'node3'];
+
+    protected $skipDriver = 'pgsql';
+
+    public static $stores = [
+        [ 'code' => 'TW001', 'name' => '天仁茗茶 01' ],
+        [ 'code' => 'TW002', 'name' => '天仁茗茶 02' ],
+        [ 'code' => 'TW003', 'name' => '天仁茗茶 03' ],
+    ];
+
+    public static $orders = [
+        'TW001' => [
+            [ 'amount' => 100, 'paid' => false ],
+            [ 'amount' => 100, 'paid' => false ],
+            [ 'amount' => 100, 'paid' => false ],
+            [ 'amount' => 100, 'paid' => false ],
+        ],
+        'TW002' => [
+            [ 'amount' => 10, 'paid' => false ],
+            [ 'amount' => 10, 'paid' => false ],
+            [ 'amount' => 10, 'paid' => false ],
+            [ 'amount' => 10, 'paid' => false ],
+        ],
+        'TW003' => [
+            [ 'amount' => 1000, 'paid' => false ],
+            [ 'amount' => 1000, 'paid' => false ],
+            [ 'amount' => 1000, 'paid' => false ],
+            [ 'amount' => 1000, 'paid' => false ],
+        ]
+    ];
 
     public function models()
     {
-        return [new StoreSchema, new OrderSchema];
+        return [
+            new StoreSchema,
+            new OrderSchema,
+        ];
     }
 
     protected function config()
     {
-        return ConfigLoader::loadFromFile("tests/apps/StoreApp/config_sqlite_file.yml", true);
+        $driver = $this->getCurrentDriverType();
+        return ConfigLoader::loadFromFile("tests/apps/StoreApp/config_{$driver}.yml", true);
     }
 
     public function orderDataProvider()
     {
-        $orders = [];
-        $orders['TW001'] = [
-            [ 'amount' => 100, 'paid' => false ],
-            [ 'amount' => 100, 'paid' => false ],
-            [ 'amount' => 100, 'paid' => false ],
-            [ 'amount' => 100, 'paid' => false ],
-        ];
-        $orders['TW002'] = [
-            [ 'amount' => 10, 'paid' => false ],
-            [ 'amount' => 10, 'paid' => false ],
-            [ 'amount' => 10, 'paid' => false ],
-            [ 'amount' => 10, 'paid' => false ],
-        ];
-        $orders['TW003'] = [
-            [ 'amount' => 1000, 'paid' => false ],
-            [ 'amount' => 1000, 'paid' => false ],
-            [ 'amount' => 1000, 'paid' => false ],
-            [ 'amount' => 1000, 'paid' => false ],
-        ];
-        return [[$orders]];
+        return [[static::$orders]];
     }
 
     public function storeDataProvider()
     {
-        $stores = [];
-        $stores[] = [ 'code' => 'TW001', 'name' => '天仁茗茶 01' ];
-        $stores[] = [ 'code' => 'TW002', 'name' => '天仁茗茶 02' ];
-        $stores[] = [ 'code' => 'TW003', 'name' => '天仁茗茶 03' ];
-        return [[$stores]];
+        return [[static::$stores]];
     }
 
-    public function assertInsertStores($storeArgs)
+    public function assertCreateStore(array $args)
+    {
+        $ret = Store::create($args);
+        $this->assertResultSuccess($ret);
+    }
+
+    public function assertCreateOrder(array $args)
+    {
+        $ret = Order::create($args); // should dispatch the shards by the store_id
+        $this->assertResultSuccess($ret);
+        $this->assertNotNull($ret->shard);
+    }
+
+
+    public function assertInsertStores(array $storeArgs)
     {
         foreach ($storeArgs as $code => $args) {
             $ret = Store::create($args);
@@ -67,7 +91,7 @@ abstract class StoreTestCase extends ModelTestCase
         }
     }
 
-    public function assertInsertOrders($orderArgsList)
+    public function assertInsertOrders(array $orderArgsList)
     {
         $orders = [];
         foreach ($orderArgsList as $storeCode => $storeOrderArgsList) {
--- tests/apps/StoreApp/Tests/StoreShardingTest.php
+++ tests/apps/StoreApp/Tests/StoreShardingTest.php
@@ -13,81 +13,8 @@ use StoreApp\Model\OrderCollection;
  * @group app
  * @group sharding
  */
-class StoreShardingTest extends ModelTestCase
+class StoreShardingTest extends \StoreApp\StoreTestCase
 {
-    protected $defaultDataSource = 'node_master';
-
-    protected $requiredDataSources = ['node_master','node1', 'node2', 'node3'];
-
-    protected $skipDriver = 'pgsql';
-
-    public function models()
-    {
-        return [
-            new StoreSchema,
-            new OrderSchema,
-        ];
-    }
-
-    protected function config()
-    {
-        $driver = $this->getCurrentDriverType();
-        return ConfigLoader::loadFromFile("tests/apps/StoreApp/config_{$driver}.yml", true);
-    }
-
-    public static $stores = [
-        [ 'code' => 'TW001', 'name' => '天仁茗茶 01' ],
-        [ 'code' => 'TW002', 'name' => '天仁茗茶 02' ],
-        [ 'code' => 'TW003', 'name' => '天仁茗茶 03' ],
-    ];
-
-    public static $orders = [
-        'TW001' => [
-            [ 'amount' => 100, 'paid' => false ],
-            [ 'amount' => 100, 'paid' => false ],
-            [ 'amount' => 100, 'paid' => false ],
-            [ 'amount' => 100, 'paid' => false ],
-        ],
-        'TW002' => [
-            [ 'amount' => 10, 'paid' => false ],
-            [ 'amount' => 10, 'paid' => false ],
-            [ 'amount' => 10, 'paid' => false ],
-            [ 'amount' => 10, 'paid' => false ],
-        ],
-        'TW003' => [
-            [ 'amount' => 1000, 'paid' => false ],
-            [ 'amount' => 1000, 'paid' => false ],
-            [ 'amount' => 1000, 'paid' => false ],
-            [ 'amount' => 1000, 'paid' => false ],
-        ]
-    ];
-
-    public function orderDataProvider()
-    {
-        return [[static::$orders]];
-    }
-
-    public function storeDataProvider()
-    {
-        return [[static::$stores]];
-    }
-
-
-    public function assertCreateStore($args)
-    {
-        $ret = Store::create($args);
-        $this->assertResultSuccess($ret);
-    }
-
-    public function assertCreateOrder($args)
-    {
-        $ret = Order::create($args); // should dispatch the shards by the store_id
-        $this->assertResultSuccess($ret);
-        $this->assertNotNull($ret->shard);
-    }
-
-
-
     /**
      * @dataProvider storeDataProvider
      */
@@ -246,6 +173,4 @@ class StoreShardingTest extends ModelTestCase
         $order2 = Order::findByPrimaryKey($orderRet->key);
         $this->assertNull($order2);
     }
-
-
 }
