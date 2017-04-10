4cef6783ae1d9e448b3d78619deff540c09654a8
c9s <yoanlin93@gmail.com>
2017-04-10 21:18:39 +0800

    Add the missing UpdateQuery

---
+++ src/Query/UpdateQuery.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Maghead\Query;
+
+use SQLBuilder\Universal\Query\UpdateQuery as BaseQuery;
+use SQLBuilder\ArgumentArray;
+
+use Maghead\Runtime\BaseRepo;
+
+class UpdateQuery
+    extends BaseQuery
+    implements Executable
+{
+    protected $repo;
+
+    /**
+     * @param BaseRepo $repo The repo object is used for executing the query.
+     */
+    public function __construct(BaseRepo $repo = null)
+    {
+        $this->repo = $repo;
+    }
+
+    public function execute()
+    {
+        return $this->repo->execute($this);
+    }
+}
