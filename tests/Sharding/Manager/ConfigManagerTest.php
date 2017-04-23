cb79f2cba198692393110854703badfbbd39dc7c
c9s <yoanlin93@gmail.com>
2017-04-24 00:08:03 +0800

    remove config copy statement

--- tests/Sharding/Manager/ConfigManagerTest.php
+++ tests/Sharding/Manager/ConfigManagerTest.php
@@ -51,7 +51,7 @@ class ShardConfigManagerTest extends StoreTestCase
         $configManager->addShardMapping($this->mapping);
         $ret = $configManager->save(self::TEST_CONFIG);
         $this->assertTrue($ret);
-        copy(self::TEST_CONFIG, 'tests/fixtures/config/testAddShardMapping.expected');
+        // copy(self::TEST_CONFIG, 'tests/fixtures/config/testAddShardMapping.expected');
         $this->assertFileEquals('tests/fixtures/config/testAddShardMapping.expected', self::TEST_CONFIG);
     }
 }
