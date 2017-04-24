440ddb491735f99cef67b085bd0921338d508451
c9s <yoanlin93@gmail.com>
2017-04-24 11:40:30 +0800

    Extract fetch helpers to PDOFetcher

--- src/Runtime/BaseRepo.php
+++ src/Runtime/BaseRepo.php
@@ -94,37 +94,6 @@ class BaseRepo
         return $this;
     }
 
-    /**
-     * Find record.
-     *
-     * @param array condition array
-     * @return BaseModel
-     */
-    // PHP 5.6 doesn't support static abstract
-    protected static function _stmFetchOne($stm, $args)
-    {
-        $stm->execute($args);
-        $obj = $stm->fetch(PDO::FETCH_CLASS);
-
-        // PDOStatement::closeCursor() frees up the connection to the server so
-        // that other SQL statements may be issued, but leaves the statement in
-        // a state that enables it to be executed again.
-        $stm->closeCursor();
-        return $obj;
-    }
-
-    /**
-     * Fetch all record.
-     *
-     * @param array condition array
-     * @return BaseModel
-     */
-    // PHP 5.6 doesn't support static abstract
-    protected static function _stmFetchAll($stm, $args)
-    {
-        $stm->execute($args);
-        return $stm->fetchAll(PDO::FETCH_CLASS);
-    }
 
     /**
      * load method loads one record from the repository with compound conditions.
