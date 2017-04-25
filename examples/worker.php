be1516fd2e7a6a0762e9ab3182cedf17d52f43f6
c9s <yoanlin93@gmail.com>
2017-04-25 21:26:31 +0800

    Add worker example

---
+++ examples/worker.php
@@ -0,0 +1,95 @@
+<?php
+require 'vendor/autoload.php';
+
+use Maghead\Sharding\QueryMapper\Gearman\GearmanQueryWorker;
+use Maghead\Manager\ConnectionManager;
+use Maghead\Config;
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
+use Monolog\Handler\ErrorLogHandler;
+
+use Maghead\ConfigLoader;
+use Maghead\Bootstrap;
+
+$config = ConfigLoader::loadFromArray([
+    'cli' => ['bootstrap' => 'vendor/autoload.php'],
+    'schema' => [
+        'auto_id' => true,
+        'base_model' => '\\Maghead\\Runtime\\BaseModel',
+        'base_collection' => '\\Maghead\\Runtime\\BaseCollection',
+        'paths' => ['tests'],
+    ],
+    'sharding' => [
+        'mappings' => [
+            // shard by hash
+            'M_store_id' => [
+                'tables' => ['orders'], // This is something that we will define in the schema.
+                'key' => 'store_id',
+                'hash' => [
+                    'target1' => 'group1',
+                    'target2' => 'group2',
+                ],
+            ],
+        ],
+        // Shards pick servers from nodes config, HA groups
+        'shards' => [
+            'group1' => [
+                'write' => [
+                    'node1_2' => ['weight' => 0.1],
+                ],
+                'read' => [
+                    'node1'   =>  ['weight' => 0.1],
+                    'node1_2' => ['weight' => 0.1],
+                ],
+            ],
+            'group2' => [
+                'write' => [
+                    'node2_2' => ['weight' => 0.1],
+                ],
+                'read' => [
+                    'node2'   =>  ['weight' => 0.1],
+                    'node2_2' => ['weight' => 0.1],
+                ],
+            ],
+        ],
+    ],
+    // data source is defined for different data source connection.
+    'data_source' => [
+        'master' => 'node1',
+        'nodes' => [
+            'node1' => [
+                'dsn' => 'sqlite:node1.sqlite',
+                'query_options' => ['quote_table' => true],
+                'driver' => 'sqlite',
+                'connection_options' => [],
+            ],
+            'node1_2' => [
+                'dsn' => 'sqlite:node1.sqlite',
+                'query_options' => ['quote_table' => true],
+                'driver' => 'sqlite',
+                'connection_options' => [],
+            ],
+            'node2' => [
+                'dsn' => 'sqlite:node2.sqlite',
+                'query_options' => ['quote_table' => true],
+                'driver' => 'sqlite',
+                'connection_options' => [],
+            ],
+            'node2_2' => [
+                'dsn' => 'sqlite:node2.sqlite',
+                'query_options' => ['quote_table' => true],
+                'driver' => 'sqlite',
+                'connection_options' => [],
+            ],
+        ],
+    ],
+]);
+Bootstrap::setup($config); // setup connection manager
+
+// create a log channel
+$logger = new Logger('worker');
+$logger->pushHandler(new StreamHandler('worker.log', Logger::DEBUG));
+$logger->pushHandler(new ErrorLogHandler(), Logger::DEBUG);
+
+$worker = new GearmanQueryWorker($config, ConnectionManager::getInstance(), null, $logger);
+$worker->run();
