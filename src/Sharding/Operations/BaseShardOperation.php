f887f88c9f85a8b27d52ac592fdeb6b61d597f48
c9s <yoanlin93@gmail.com>
2017-04-29 10:31:46 +0800

    Add BaseShardOperation class file

---
+++ src/Sharding/Operations/BaseShardOperation.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Maghead\Sharding\Operations;
+
+use Maghead\Config;
+use Maghead\Manager\ConnectionManager;
+use Maghead\Manager\DatabaseManager;
+use Maghead\Manager\DataSourceManager;
+use Maghead\Sharding\Manager\ShardManager;
+
+class BaseShardOperation
+{
+    protected $config;
+
+    protected $instanceManager;
+
+    protected $dataSourceManager;
+
+    protected $shardManager;
+
+    public function __construct(Config $config)
+    {
+        $this->config = $config;
+        $this->instanceManager = new ConnectionManager($config->getInstances());
+        $this->dataSourceManager = new DataSourceManager($config->getDataSources());
+        $this->shardManager = new ShardManager($this->config, $this->dataSourceManager);
+    }
+}
