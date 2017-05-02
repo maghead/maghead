f2bdd30cf105a45f4e4b4f51de8dfb4544d81246
c9s <yoanlin93@gmail.com>
2017-05-02 19:02:35 +0800

    always init config for MigrateCommandTest

--- tests/Commands/MigrateCommandTest.php
+++ tests/Commands/MigrateCommandTest.php
@@ -12,6 +12,13 @@ class MigrateCommandsTest extends CommandTestCase
         return new Console;
     }
 
+    public function setUp()
+    {
+        parent::setUp();
+        copy('tests/config/mysql.yml', 'tests/config/mysql.tmp.yml');
+        $this->app->run(['maghead','use','tests/config/mysql.tmp.yml']);
+    }
+
     public function testMigrateStatusCommand()
     {
         $this->app->run(array('maghead','migrate','status'));
