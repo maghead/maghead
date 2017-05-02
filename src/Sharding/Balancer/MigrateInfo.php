9d502875d1b69898d62177602a2425ab30c211b2
c9s <yoanlin93@gmail.com>
2017-05-02 18:09:41 +0800

    Add MigrateInfo status

--- src/Sharding/Balancer/MigrateInfo.php
+++ src/Sharding/Balancer/MigrateInfo.php
@@ -16,17 +16,43 @@ class MigrateInfo {
      */
     public $to;
 
+    public $status = null;
+
+    const SUCCEED = TRUE;
+
+    const FAILED = FALSE;
+
     public function __construct(Shard $shard, Chunk $chunk, array $keys) {
         $this->to    = $shard;
         $this->chunk = $chunk;
         $this->keys  = $keys;
     }
 
+    public function setFailed()
+    {
+        $this->status = self::FAILED;
+    }
+
+    public function setSucceed()
+    {
+        $this->status = self::SUCCEED;
+    }
+
+    public function succeed()
+    {
+        return $this->status === self::SUCCEED;
+    }
+
+    public function failed()
+    {
+        return $this->status === self::FAILED;
+    }
+
     public function __debugInfo()
     {
         return [
-            'toShard' => $this->to->id,
-            'chunk' => $this->chunk->__debugInfo(),
+            'toShard' => $this->to,
+            'chunk' => $this->chunk,
             'keys' => $this->keys,
         ];
     }
