71ee0877b3f30ed599a4c51eb39f0a2af8d7ab2d
c9s <yoanlin93@gmail.com>
2017-05-04 15:10:50 +0800

    Move DeflatorTest to src

---
+++ src/DeflatorTest.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace Maghead;
+
+use Maghead\Deflator;
+use DateTime;
+
+/**
+ * @group hydrate
+ */
+class DeflatorTest extends \PHPUnit\Framework\TestCase
+{
+    public function testInt()
+    {
+        $this->assertEquals(1, Deflator::deflate('1', 'int'));
+    }
+
+    public function testDatetime()
+    {
+        $d = new DateTime;
+        $dstr = Deflator::deflate($d, 'DateTime');
+        $this->assertEquals($d->format(DateTime::ATOM), $dstr);
+        $this->assertEquals(null, Deflator::deflate('', 'DateTime'));
+        $this->assertEquals(null, Deflator::deflate(null, 'DateTime'));
+    }
+
+    public function testFloat()
+    {
+        $this->assertEquals(1.1, Deflator::deflate('1.1', 'float'));
+    }
+
+
+    public function stringTestDataProvider()
+    {
+        return [
+            ['1', 1],
+            ['1.1', 1.1],
+            ['', false],
+        ];
+    }
+
+    /**
+     * @dataProvider stringTestDataProvider
+     */
+    public function testStr($exp, $input)
+    {
+        $this->assertEquals($exp, Deflator::deflate($input, 'str'));
+    }
+
+    public function boolTestDataProvider()
+    {
+        return [
+            [1, 1],
+            [0, 0],
+            [null, null],
+            [false, ''],
+            [false, '0'],
+            [true, '1'],
+            [true, 'true'],
+            [false, 'false'],
+        ];
+    }
+
+    /**
+     * @dataProvider boolTestDataProvider
+     */
+    public function testDeflateBool($exp, $input)
+    {
+        $this->assertEquals($exp, Deflator::deflate($input, 'bool'));
+    }
+}
