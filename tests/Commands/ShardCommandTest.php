2f09709ce98dbba6d2ebcd618c3c9268116edda6
c9s <yoanlin93@gmail.com>
2017-05-07 01:11:16 +0800

    improve shard command workflow tests

--- tests/Commands/ShardCommandTest.php
+++ tests/Commands/ShardCommandTest.php
@@ -1,68 +1,70 @@
 <?php
 
-use CLIFramework\Testing\CommandTestCase;
-use Maghead\Console;
+use Maghead\Testing\CommandWorkFlowTestCase;
 
 /**
  * @group command
  */
-class ShardCommandsTest extends CommandTestCase
+class ShardCommandsTest extends CommandWorkFlowTestCase
 {
-    public function setupApplication()
-    {
-        return new Console;
-    }
-
-    public function setUp()
-    {
-        parent::setUp();
-        $db = getenv('DB') ?: "sqlite";
-        copy("tests/config/$db.yml", "tests/config/tmp.yml");
-        $this->app->run(['maghead','use','tests/config/tmp.yml']);
-        if ($db !== "mysql") {
-            return $this->markTestSkipped('sqlite migration is not supported.');
-        }
-    }
+    public $onlyDriver = 'mysql';
 
     public function testShardMappingCreate()
     {
-        $this->app->run(['maghead', 'shard',
-            'mapping', 'add', '--hash', '-s', 's1', '-s', 's2', '--key', 'store_id', 'store_key'
+        $this->expectOutputRegex('/Chunk:\s*\d+/');
+        $ret = $this->app->run(['maghead', 'shard',
+            'mapping', 'add', '--hash', '-s', 'node1', '-s', 'node2', '--key', 'store_id', 'store_key'
         ]);
-
-        $this->app->run(['maghead', 'shard',
-            'mapping', 'remove', 'store_key'
-        ]);
+        $this->assertTrue($ret);
     }
 
-
+    /**
+     * @depends testShardMappingCreate
+     */
     public function testShardAllocate()
     {
-        $this->app->run(['maghead', 'shard',
-            'mapping', 'add', '--hash', '-s', 's1', '-s', 's2', '--key', 'store_id', 'store_key'
-        ]);
-
-        $this->app->run(['maghead', 'shard', 'allocate', '--mapping', 'store_key', '--instance', 'local', 'a11']);
-
-        $this->app->run(['maghead', 'shard',
-            'mapping', 'remove', 'store_key'
-        ]);
+        $ret = $this->app->run(['maghead', 'shard', 'allocate', '--mapping', 'store_key', '--instance', 'local', 'a11']);
+        $this->assertTrue($ret);
     }
 
-    public function testShardCloneAndPrune()
+    /**
+     * @depends testShardAllocate
+     */
+    public function testShardClone()
     {
-        // $this->expectOutputRegex('/Copying TABLE/');
-
-        $this->app->run(['maghead', 'shard',
-            'mapping', 'add', '--hash', '-s', 's1', '-s', 's2', '--key', 'store_id', 'store_key'
-        ]);
-
-        $this->app->run(['maghead', 'shard', 'clone', '--drop-first', '--mapping', 'store_key', '--instance', 'local', 'master', 'a11']);
-
-        $this->app->run(['maghead', 'shard', 'prune', '--mapping', 'store_key', 'a11']);
-
-        $this->app->run(['maghead', 'shard',
-            'mapping', 'remove', 'store_key'
-        ]);
+        $this->expectOutputRegex('/#...done./');
+        $ret = $this->app->run(['maghead', 'shard', 'clone', '--drop-first', '--mapping', 'store_key', '--instance', 'local', 'master', 'a11']);
+        $this->assertTrue($ret);
     }
+
+    public function nodeDataProvider()
+    {
+        return [
+            ['a11'],
+            ['node1'],
+            ['node2'],
+            ['node3'],
+        ];
+    }
+
+    /**
+     * @depends testShardClone
+     * @dataProvider nodeDataProvider
+     */
+    public function testShardPrune($node)
+    {
+        $ret = $this->app->run(['maghead', 'shard', 'prune', '--mapping', 'store_key', $node]);
+        $this->assertTrue($ret);
+    }
+
+
+    /**
+     * @depends testShardPrune
+     */
+    public function testShardMappingRemove()
+    {
+        $ret = $this->app->run(['maghead', 'shard', 'mapping', 'remove', 'store_key' ]);
+        $this->assertTrue($ret);
+    }
+
 }
