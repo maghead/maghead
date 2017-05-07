ee35883ebb7e7950ac28a598ab9fd05710a05c9e
c9s <yoanlin93@gmail.com>
2017-05-07 08:08:38 +0800

    Add the missing file...

--- scripts/lazy.emb.php
+++ scripts/lazy.emb.php
@@ -1,6 +1,7 @@
 <?php
-if ( ! extension_loaded('fileutil') && ! function_exists('futil_pathsplit') ) {
-    require 'phar://lazy.phar/FileUtil.php';
+if (! extension_loaded('fileutil') && ! function_exists('futil_pathsplit') ) {
+    require 'phar://maghead.phar/FileUtil.php';
 }
-$console = new Maghead\Console\Application;
-$console->run($argv);
+use Maghead\Console\Application;
+$app = new Application;
+$app->run($argv);
---
+++ src/Console/Application.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Maghead\Console;
+
+class Application extends \CLIFramework\Application
+{
+    const NAME = 'Maghead';
+    const VERSION = '4.0.x';
+
+    public function brief()
+    {
+        return 'Maghead ORM';
+    }
+
+    public function init()
+    {
+        parent::init();
+
+        /*
+         * Command for initialize related file structure
+         */
+        $this->command('init');
+
+        /*
+         * Command for building config file.
+         */
+        $this->command('use');
+        // $this->command('init-conf', 'Maghead\\Command\\InitConfCommand');
+
+        $this->command('schema'); // the schema command builds all schema files and shows a diff after building new schema
+        $this->command('basedata');
+        $this->command('sql');
+        $this->command('diff');
+        $this->command('migrate');
+        $this->command('meta');
+        $this->command('version');
+        $this->command('db');
+        $this->command('shard');
+        $this->command('table');
+        $this->command('index');
+        $this->command('shard');
+    }
+
+    public static function getInstance()
+    {
+        static $self;
+        if ($self) {
+            return $self;
+        }
+
+        return $self = new self();
+    }
+}
+
