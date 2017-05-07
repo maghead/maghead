1ca5f9aaa836e3e9ba086b56296c0ac7d74ab076
c9s <yoanlin93@gmail.com>
2017-05-07 18:58:12 +0800

    Extract queryOptions

--- src/Runtime/Config/MongoConfigLoader.php
+++ src/Runtime/Config/MongoConfigLoader.php
@@ -6,16 +6,19 @@ use MongoDB\Client;
 
 class MongoConfigLoader
 {
+
+    public static $queryOptions = [
+        'typeMap' => [
+            'array' => 'array',
+            'document' => 'array',
+            'root' => 'array',
+        ]
+    ];
+
     public static function load($appId, Client $client)
     {
         $collection = $client->maghead->configs;
-        $doc = $collection->findOne(['appId' => $appId], [
-            'typeMap' => [
-                'array' => 'array',
-                'document' => 'array',
-                'root' => 'array',
-            ],
-        ]);
+        $doc = $collection->findOne(['appId' => $appId], static::$queryOptions);
         if ($doc) {
             return new Config($doc['stash']);
         }
