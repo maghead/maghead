97f0ce3b48d778e8b16c32656e9c26c6b7b73140
c9s <yoanlin93@gmail.com>
2017-05-07 18:00:48 +0800

    remove another unnecessary if

--- src/Runtime/SeedBuilder.php
+++ src/Runtime/SeedBuilder.php
@@ -21,20 +21,19 @@ class SeedBuilder
 
     public function buildSchemaSeeds(BaseSchema $schema)
     {
-        if ($modelClass = $schema->getModelClass()) {
+        $seedData = $schema->seeds();
+        if (!empty($seedData)) {
+            $modelClass = $schema->getModelClass()
             $this->logger->info("Creating base data of $modelClass");
-            $seedList = $schema->seeds();
-            if (!empty($seedList)) {
-                foreach ($seedList as $seedArg) {
-                    if (!is_array($seedArg)) {
-                        throw new InvalidArgumentException('Seeds data needs to be plain array.');
-                    }
+            foreach ($seedData as $seedArg) {
+                if (!is_array($seedArg)) {
+                    throw new InvalidArgumentException('Seeds data needs to be plain array.');
+                }
 
-                    $this->logger->info("Seeding: " . ArrayUtils::describe($seedArg));
-                    $ret = $modelClass::create($seedArg);
-                    if ($ret->error) {
-                        $this->logger->error("ERROR: {$ret->message}");
-                    }
+                $this->logger->info("Seeding: " . ArrayUtils::describe($seedArg));
+                $ret = $modelClass::create($seedArg);
+                if ($ret->error) {
+                    $this->logger->error("ERROR: {$ret->message}");
                 }
             }
         }
