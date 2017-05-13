fa4314714e880efb4387f26817fc25b8852a5ecd
c9s <yoanlin93@gmail.com>
2017-05-13 13:19:19 +0800

    FileConfigWriter should not include Symlink anchor filename

--- src/Runtime/Config/FileConfigWriter.php
+++ src/Runtime/Config/FileConfigWriter.php
@@ -7,24 +7,16 @@ use Symfony\Component\Yaml\Yaml;
 
 class FileConfigWriter
 {
-    const ANCHOR_FILENAME = '.maghead-cli.yml';
-
     public static $inlineLevel = 5;
 
     public static $indentSpaces = 2;
 
     public static $currentConfig;
 
-    public static function write(Config $config, $targetFile = null)
+    public static function write(Config $config, $targetFile)
     {
-        if (!$targetFile) {
-            if (!file_exists(self::ANCHOR_FILENAME)) {
-                throw new Exception('symbol link '.self::ANCHOR_FILENAME.' does not exist.');
-            }
-            $targetFile = readlink(self::ANCHOR_FILENAME);
-        }
-        if (!$targetFile && !file_exists($targetFile)) {
-            throw new Exception('Missing target config file. incorrect symbol link.');
+        if (is_link($targetFile)) {
+            $targetFile = readlink($targetFile);
         }
 
         $yaml = Yaml::dump($config->getArrayCopy(), self::$inlineLevel, self::$indentSpaces);
