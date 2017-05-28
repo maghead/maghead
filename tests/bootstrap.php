e3bc30cfb1216b4618cfd378cd3ff890f45a1bca
c9s <yoanlin93@gmail.com>
2017-05-29 00:36:05 +0800

    add phpunit polyfill

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,2 +1,7 @@
 <?php
 require 'vendor/autoload.php';
+
+// This should work for HHVM on travis
+if (!class_exists('PHPUnit\\Framework\\TestCase')) {
+    class_alias('PHPUnit_Framework_TestCase', 'PHPUnit\\Framework\\TestCase');
+}
