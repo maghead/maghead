f36d0b44b6e047e663c8a21988c73d36b55dda0d
c9s <yoanlin93@gmail.com>
2017-05-29 10:04:23 +0800

    fix

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -3,6 +3,6 @@ require 'vendor/autoload.php';
 
 // PHPUnit TestCase polyfill
 // This should work for HHVM on travis
-if (!class_exists('PHPUnit\Framework\TestCase') && class_exists('PHPUnit_Framework_TestCase')) {
-    class_alias('PHPUnit_Framework_TestCase', 'PHPUnit\Framework\TestCase');
+if (!class_exists('\PHPUnit_Framework_TestCase') && class_exists('\PHPUnit\Framework\TestCase')) {
+    class_alias('\PHPUnit\Framework\TestCase', '\PHPUnit_Framework_TestCase');
 }
