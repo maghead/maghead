e9b1094c1eb416a1dd9dc30a149fccf74245b6f8
c9s <yoanlin93@gmail.com>
2017-06-11 19:40:21 +0900

    test has many relationship

--- examples/books/Model/AuthorSchemaTest.php
+++ examples/books/Model/AuthorSchemaTest.php
@@ -4,11 +4,19 @@ namespace AuthorBooks\Model;
 
 use PHPUnit\Framework\TestCase;
 
+use Maghead\Schema\Relationship\HasMany;
+use Maghead\Schema\Relationship\Relationship;
+
+
 class AuthorSchemaTest extends TestCase
 {
     public function testForeignRelationship()
     {
         $proxy = new AuthorSchemaProxy;
         $this->assertNotEmpty($proxy->relations);
+
+        $rel = $proxy->relations['author_books'];
+        $this->assertInstanceOf(Relationship::class, $rel);
+        $this->assertEquals('author_books', $rel->accessor);
     }
 }
