799f8dbc747a01c3bbe5f343df40e5991af68fe5
c9s <yoanlin93@gmail.com>
2017-06-13 00:34:24 +0900

    add mysql 5.6 upgrade note for timestamps

--- src/Schema/Mixin/MetadataMixinSchema.php
+++ src/Schema/Mixin/MetadataMixinSchema.php
@@ -9,6 +9,46 @@ use Magsql\Raw;
 
 class MetadataMixinSchema extends MixinDeclareSchema
 {
+    /*
+    The following SQL is MySQL 5.5 compatible:
+
+        DROP TABLE IF EXISTS t1;
+        CREATE TABLE t1 (
+          `created_at` TIMESTAMP NULL DEFAULT 0,
+          `updated_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+        );
+
+    The following SQL is MySQL 5.6 compatible (multiple CURRENT_TIMESTAMP column is supported)
+
+        DROP TABLE IF EXISTS t1;
+        CREATE TABLE t1 (
+          `created_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+          `updated_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+        );
+            
+    In MySQL, timestamp columns are default to "NOT NULL"
+
+
+    Here is the upgrade note from MySQL 5.6
+
+    In MySQL, the TIMESTAMP data type differs in nonstandard ways from other data
+    types:
+
+    TIMESTAMP columns not explicitly declared with the NULL attribute are assigned
+        the NOT NULL attribute. (Columns of other data types, if not explicitly
+        declared as NOT NULL, permit NULL values.) Setting such a column to NULL
+        sets it to the current timestamp.
+
+    The first TIMESTAMP column in a table, if not declared with the NULL attribute
+    or an explicit DEFAULT or ON UPDATE clause, is automatically assigned the
+    DEFAULT CURRENT_TIMESTAMP and ON UPDATE CURRENT_TIMESTAMP attributes.
+
+    TIMESTAMP columns following the first one, if not declared with the NULL
+    attribute or an explicit DEFAULT clause, are automatically assigned DEFAULT
+    '0000-00-00 00:00:00' (the “zero” timestamp). For inserted rows that specify
+    no explicit value for such a column, the column is assigned '0000-00-00 00:00:00' and no warning occurs.
+
+     */
     public function schema()
     {
         // MySQL: ts TIMESTAMP
