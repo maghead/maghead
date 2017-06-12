c34bd13134ce43ac2c3061793d368511d87be3c6
c9s <yoanlin93@gmail.com>
2017-06-12 21:55:27 +0900

    Add MysqlBuilderTest

---
+++ src/TableBuilder/MysqlBuilderTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Maghead\TableBuilder;
+
+use Maghead\Testing\ModelTestCase;
+
+use AuthorBooks\Model\AuthorSchema;
+use AuthorBooks\Model\BookSchema;
+
+class MysqlBuilderTest extends ModelTestCase
+{
+    protected $onlyDriver = 'mysql';
+
+    public function models()
+    {
+        return [new BookSchema];
+    }
+
+    public function test()
+    {
+        $schema = new BookSchema;
+        $b = new MysqlBuilder($this->conn->getQueryDriver());
+        $sql = $b->buildColumnSql($schema, $schema->getColumn('updated_at'));
+        $this->assertContains('DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP', $sql);
+    }
+}
