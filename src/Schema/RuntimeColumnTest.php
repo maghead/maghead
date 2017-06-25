75252735f541e805681c8efde3307f8bc473b951
c9s <yoanlin93@gmail.com>
2017-06-26 00:00:23 +0800

    Add basic valid values tests

---
+++ src/Schema/RuntimeColumnTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Maghead\Schema;
+
+use PHPUnit\Framework\TestCase;
+
+class RuntimeColumnTest extends TestCase
+{
+    public function testValidate()
+    {
+        $column = new RuntimeColumn("foo", [
+            "isa" => "str",
+            "type" => "varchar",
+            "primary" => false,
+            "unsigned" => false,
+            "notNull" => true,
+            "validValues" => [
+                'admin',
+                'user',
+                'guest',
+            ],
+        ]);
+
+        $ret = $column->validate("foo", [ ]);
+        $this->assertFalse($ret['valid']);
+
+        $ret = $column->validate("admin", [ ]);
+        $this->assertNull($ret);
+    }
+}
+
+
+
